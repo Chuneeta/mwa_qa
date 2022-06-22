f4f680c5c563be9019ed7193c9a030908366a74d
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-06-22 13:14:37 +0800

    added unittest to tests fucntion to extract gains for a specific receiver

--- mwa_clysis/tests/test_read_csolutions.py
+++ mwa_clysis/tests/test_read_csolutions.py
@@ -74,18 +74,62 @@ class TestCsoln(unittest.TestCase):
         				0.05096334+0.02108187j, -0.24661253+1.2017221j ])
 		np.testing.assert_almost_equal(ngains[0, 0, 100, :], expected)
 
+	def test_select_gains(self):
+		c = rc.Csoln(calfile)
+		gains = c._select_gains(norm = True)
+		expected = np.array([-0.6544542 +0.9396147j ,  0.04411516-0.05776248j,
+                        	0.05096334+0.02108187j, -0.24661253+1.2017221j ])
+		np.testing.assert_almost_equal(gains[0, 0, 100, :], expected)
+		gains = c._select_gains(norm = False)
+		expected = np.array([-0.03619801+0.8295782j ,  0.02484267+0.00862688j,
+                      		0.00525203+0.00732957j,  0.23554221+0.8980131j ])
+		np.testing.assert_almost_equal(gains[0, 0, 100, :], expected)
+
 	def test_amplitudes(self):
 		c = rc.Csoln(calfile)
 		amps = c.amplitudes()
 		self.assertTrue(amps.shape == (1, 3, 768, 4))
 		self.assertTrue(np.isnan(np.abs(amps[0, 0, 0, :])).all())
 		expected = np.array([1.1450703 , 0.07268185, 0.05515167, 1.2267656 ])
-		np.testing.assert_almost_equal(amps[0, 0, 100, :], expected)
-	
+		np.testing.assert_almost_equal(amps[0, 0, 100, :], expected, decimal = 6) 
+		amps = c.amplitudes(norm = False)
+		expected = np.array([0.83036757, 0.02629794, 0.00901701, 0.92838985])		
+		np.testing.assert_almost_equal(amps[0, 0, 100, :], expected, decimal = 6)
+
 	def test_phases(self):
 		c = rc.Csoln(calfile)
 		phases = c.phases()
 		self.assertTrue(phases.shape == (1, 3, 768, 4))
 		self.assertTrue(np.isnan(np.abs(phases[0, 0, 0, :])).all())
 		expected = np.array([124.85773 , -52.629803,  22.47328 , 101.597 ])
-		np.testing.assert_almost_equal(phases[0, 0, 100, :], expected, decimal=3)
+		np.testing.assert_almost_equal(phases[0, 0, 100, :], expected, decimal = 3)
+		phases = c.phases(norm = False)
+		expected = np.array([92.498474, 19.150087, 54.376358, 75.30281 ])
+		np.testing.assert_almost_equal(phases[0, 0, 100, :], expected, decimal = 6)
+
+	def test_gains_for_tile(self):
+		c = rc.Csoln(calfile, metafits=metafits)
+		gains = c.gains_for_tile('Tile104')
+		self.assertTrue(gains.shape == (1, 1, 768, 4))
+		expected = np.array([-0.6544542 +0.9396147j ,  0.04411516-0.05776248j,
+        					0.05096334+0.02108187j, -0.24661253+1.2017221j ])
+		np.testing.assert_almost_equal(gains[0, 0, 100, :], expected)
+		gains = c.gains_for_tile('Tile104', norm = False)
+		expected = np.array([-0.03619801+0.8295782j ,  0.02484267+0.00862688j,
+        			0.00525203+0.00732957j,  0.23554221+0.8980131j ])
+		np.testing.assert_almost_equal(gains[0, 0, 100, :], expected)
+
+	def test_gains_for_receiver(self):
+		c = rc.Csoln(calfile, metafits=metafits)
+		gains_dict = c.gains_for_receiver(10)
+		self.assertTrue(len(list(gains_dict.keys())) == 3)
+		self.assertEqual(list(gains_dict.keys()), ['Tile104', 'Tile103', 'Tile102'])
+		expected = np.array([-0.6544542 +0.9396147j ,  0.04411516-0.05776248j,
+                            0.05096334+0.02108187j, -0.24661253+1.2017221j ])
+		np.testing.assert_almost_equal(gains_dict['Tile104'][0, 0, 100, :], expected)
+		gains_dict = c.gains_for_receiver(10, norm = False)
+		expected = np.array([-0.03619801+0.8295782j ,  0.02484267+0.00862688j,
+                    0.00525203+0.00732957j,  0.23554221+0.8980131j ])
+		np.testing.assert_almost_equal(gains_dict['Tile104'][0, 0, 100, :], expected)
+		
+
