58b5050f4ecd2f94f9d574d5a292241f5e4acb21
Nunhokee Chuneeta <Chuneeta@users.noreply.github.com>
2022-06-29 10:10:19 +0800

    Deleted mwa_clysis

--- mwa_clysis/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-__init__.py file from mwa_clysis
-"""
-
-from mwa_clysis import read_csoln, read_csolutions, read_metafits, get_stats
--- mwa_clysis/data/__init__.py
+++
@@ -1,2 +0,0 @@
-# init file for mwa_clysis/data
-DATA_PATH = __path__[0]
