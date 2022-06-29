fc6d40d324b9664e2f6f94123f2b2503646c31f8
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-06-29 12:25:53 +0800

    changed mwa_clysis to mwa_qa

--- mwa_qa/__init__.py
+++ mwa_qa/__init__.py
@@ -2,4 +2,4 @@
 __init__.py file from mwa_clysis
 """
 
-from mwa_clysis import read_csoln, read_csolutions, read_metafits, get_stats
+from mwa_qa import read_csoln, read_csolutions, read_metafits, get_stats
