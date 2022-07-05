c59d5f168bb37e241834ce96d35974e036c1cbca
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-07-05 14:51:26 +0800

    utility function to read fitsimages

---
+++ mwa_qa/image_utils.py
@@ -0,0 +1,36 @@
+from astropy.io import fits
+import numpy as np
+
+def data(image):
+	data = fits.open(image)[0].data
+	return data.squeeze()
+
+def header(image):
+	return fits.open(image)[0].header
+
+def image_size(image):
+	hdr = header(image)
+	return (hdr['NAXIS1'], hdr['NAXIS2'])
+
+def mean(image):
+	return np.nanmean(data(image))
+
+def rms(image):
+	nxpix, nypix = image_size(image)
+	return np.sqrt(np.nansum(data(image) ** 2) / (nxpix * nypix) )
+
+def std(image):
+	return np.nanstd(data(image))
+
+def select_box(image, xpix, ypix):
+	_d = data(image)
+	return _d[0:xpix , 0:ypix]
+
+def rms_for(image, xpix, ypix):
+	pix_box = select_box(image, xpix, ypix)
+	return np.sqrt(np.nanmean(pix_box ** 2))
+
+def std_for(image, xpix, ypix):
+	pix_box = select_box(image, xpix, ypix)
+	return np.nanstd(pix_box)
+
