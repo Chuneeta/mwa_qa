7ca95b7230694bc518271b17f96aab3898b04c20
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-07-07 16:05:34 +0800

    reorganising the metrics object

--- mwa_qa/cal_metrics.py
+++ mwa_qa/cal_metrics.py
@@ -3,109 +3,105 @@ from mwa_qa import read_csolutions as rc
 from mwa_qa import json_utils as ju
 from collections import OrderedDict
 import numpy as np
-import json
-
 
 class CalMetrics(object):
-    def __init__(self, calfile, metafits=None, pol='X'):
-        """
-        Object that takes in .fits containing the calibration solutions file readable by astropy
-        and initializes them as global varaibles
-        - calfile : .fits file containing the calibration solutions
-        - metafits : Metafits with extension *.metafits or _ppds.fits containing information
-                     on an observation done with MWA
-        - pol : Polarization, can be either 'X' or 'Y'. It should be specified so that information associated 
-        with the given pol is provided. Default is X.
-        """
-        self.calfile = calfile
-        self.Csoln = rc.Csoln(calfile, metafits = metafits, pol = pol)
-        self.Metafits = rm.Metafits(metafits, pol)
+	def __init__(self, calfile, metafits=None, pol='X'):
+		"""
+		Object that takes in .fits containing the calibration solutions file readable by astropy
+		and initializes them as global varaibles
+		- calfile : .fits file containing the calibration solutions
+		- metafits : Metafits with extension *.metafits or _ppds.fits containing information 
+					 on an observation done with MWA
+		- pol : Polarization, can be either 'X' or 'Y'. It should be specified so that information associated 
+				with the given pol is provided. Default is X.
+		"""
+		self.calfile = calfile
+		self.Csoln = rc.Csoln(calfile, metafits = metafits, pol = pol)
+		self.Metafits = rm.Metafits(metafits, pol)
 
-    def variance_for_tilepair(self, tile_pair, norm = True):
-        """
-        Returns variance across frequency for the given tile pair
-         - tile_pair : Tile pair or tuple of tile numbers e.g (102, 103)
-         - norm : boolean, If True returns normalized gains else unormalized gains.
-                 Default is set to True.
-        """
-        gains = self.Csoln.gains_for_tilepair(tile_pair, norm = norm)
-        return np.nanvar(gains, axis = 2)
+	def variance_for_tilepair(self, tile_pair, norm = True):
+		"""
+		Returns variance across frequency for the given tile pair
+		- tile_pair : Tile pair or tuple of tile numbers e.g (102, 103)
+		- norm : boolean, If True returns normalized gains else unormalized gains.
+				 Default is set to True.
+		"""
+		gains = self.Csoln.gains_for_tilepair(tile_pair, norm = norm)
+		return np.nanvar(gains, axis = 2)
 
-    def variance_for_baselines_less_than(self, uv_cut, norm=True):
-        """
-        Returns bls shorter than the specified cut and the variances calculated across frequency for
-        each of the antenna pair
-        - baseline_cut : Baseline cut in metres, will use only baselines shorter than the given value
-        - norm : boolean, If True returns normalized gains else unormalized gains.
-                 Default is set to True.
-        """
-        baseline_dict = self.Metafits.get_baselines_less_than(uv_cut)
-        bls = list(baseline_dict.keys())
-        _sh = self.Csoln.gains().shape
-        variances = np.zeros((_sh[0], len(bls), _sh[3]))
-        for i , bl in enumerate(bls):
-            variances[:, i, :] = self.variance_for_tilepair(bl, norm = norm)[:, :, :]
-        return bls, variances
+	def variance_for_baselines_less_than(self, uv_cut, norm=True):
+		"""
+		Returns bls shorter than the specified cut and the variances calculated across frequency for
+		each of the antenna pair
+		- baseline_cut : Baseline cut in metres, will use only baselines shorter than the given value
+		- norm : boolean, If True returns normalized gains else unormalized gains.
+				 Default is set to True.
+		"""
+		baseline_dict = self.Metafits.get_baselines_less_than(uv_cut)
+		bls = list(baseline_dict.keys())
+		_sh = self.Csoln.gains().shape
+		variances = np.zeros((_sh[0], len(bls), _sh[3]))
+		for i , bl in enumerate(bls):
+			variances[:, i, :] = self.variance_for_tilepair(bl, norm = norm)[:, :, :]
+		return bls, variances
 
-    def skewness_across_baselines(self, uv_cut, norm=True):
-        """
-        Evaluates the Pearson skewness 3 * (mean - median) / std across the variances 
-        averaged over baseliness shorter than the given length
-        - uv_cut : Baseline cut in metres, will use only baselines shorter than the given value
-        - norm : boolean, If True returns normalized gains else unormalized gains.
-        Default is set to True.
-        """
-        _, variances = self.variance_for_baselines_less_than(uv_cut, norm = norm)
-        skewness = (3 * ( np.nanmean(variances, axis = 1) - np.nanmedian(variances, axis = 1))) / np.nanstd(variances, axis=1)
-        return skewness
+	def skewness_across_uvcut(self, uv_cut, norm=True):
+		"""
+		Evaluates the Pearson skewness 3 * (mean - median) / std across the variances 
+		averaged over baseliness shorter than the given length
+		- uv_cut : Baseline cut in metres, will use only baselines shorter than the given value
+		- norm : boolean, If True returns normalized gains else unormalized gains.
+		Default is set to True.
+		"""
+		_, variances = self.variance_for_baselines_less_than(uv_cut, norm = norm)
+		skewness = (3 * ( np.nanmean(variances, axis = 1) - np.nanmedian(variances, axis = 1))) / np.nanstd(variances, axis=1)
+		return skewness
 
-    def get_receivers(self, n = 16):
-        """
-        Returns the receivers connected to the various tiles in the array
-        - n : Number of receivers in the array. Optional, enabled if metafits is not provided.
-              Default is 16.
-        """
-        if self.Metafits.metafits is None:
-            receivers = list(np.arange(1, n + 1))
-        else:
-            receivers = self.Metafits.receivers()
-        return receivers
+	def get_receivers(self, n = 16):
+		"""
+		Returns the receivers connected to the various tiles in the array
+		- n : Number of receivers in the array. Optional, enabled if metafits is not provided.
+			  Default is 16.
+		"""
+		if self.Metafits.metafits is None:
+			receivers = list(np.arange(1, n + 1))
+		else:
+			receivers = self.Metafits.receivers()
+		return receivers
 
-    def _initialize_metrics_dict(self):
-        """
-        Initializes the metric dictionary with some of the default parameters
-        """
-        metrics = OrderedDict()
-        _, freqs, _ = self.Csoln.freqs_info()
-        _, tile_ids, tile_flags = self.Csoln.tile_info()
-        metrics['pols'] = ['XX', 'XY', 'YX', 'YY']
-        metrics['freqs'] = freqs
-        metrics['tile_ids'] = tile_ids
-        metrics['uvcut'] = 40 # temporarily, will be adjusted
-        return metrics
+	def _initialize_metrics_dict(self):
+		"""
+		Initializes the metric dictionary with some of the default parameters
+		"""
+		self.metrics = OrderedDict()
+		_, freqs, _ = self.Csoln.freqs_info()
+		_, tile_ids, tile_flags = self.Csoln.tile_info()
+		tile_numbers = [int(tid.strip('Tile')) for tid in tile_ids]
+		hdr = self.Csoln.header(0)
+		pols = ['XX', 'XY', 'YX', 'YY']
+		self.metrics['pols'] = pols
+		self.metrics['uvcut'] = hdr['UVW_MIN']
+		self.metrics['niter'] = hdr['MAXITER']
+		for i, p in enumerate(pols):
+			self.metrics[p] = OrderedDict()
+			for j, tn in enumerate(tile_numbers):
+				self.metrics[p][tn] = OrderedDict()
 
-    def run_metrics(self):
-        metrics = OrderedDict()
-        metrics['mean_freq'] = np.nanmean(self.Csoln.amplitudes(), axis = 2)
-        metrics['median_freq'] = np.nanmedian(self.Csoln.amplitudes(), axis = 2)
-        metrics['variance_freq'] = np.nanvar(self.Csoln.amplitudes(), axis = 2)
-        metrics['rms_freq'] = np.sqrt(np.nanmean(np.abs(gains) ** 2, axis = 2))
-        metrics['mean_time'] = np.nanmean(self.Csoln.amplitudes(), axis = 0)
-        metrics['median_time'] = np.nanmedian(self.Csoln.amplitudes(), axis = 0)
-        metrics['variance_time'] = np.nanvar(self.Csoln.amplitudes(), axis = 0)
-        metrics['rms_time'] = np.sqrt(np.nanmean(np.abs(gains) ** 2, axis = 0))
-        metrics['skewness_across_baselines'] = self.skewness_across_baselines(metrics['uvcut'])
-        return metrics
-
-    def write_metrics(self, metrics, filename = None):
-        """
-        Writing metrics to output files
-        - metrics : dictionary
-        - filename : Output file
-        """
-        if filename is None:
-            filename = self.calfile.replace('.fits', '_metrics.json')
-        metrics = self.run_metrics()
-        utils.write_metrics(metrics, filename)
-
-
+	def run_metrics(self):
+		self._initialize_metrics_dict()
+		pols = self.metrics['pols']
+		tile_keys = list(self.metrics[pols[0]].keys())
+		for i, p in enumerate(pols):
+			for j, tn in enumerate(tile_keys):
+				gain_amplitudes = self.Csoln.amplitudes()[:, j, :, i]
+				self.metrics[p][tn]['mean_amp_freq'] = np.nanmean(gain_amplitudes, axis = 1).tolist()  
+				self.metrics[p][tn]['median_amp_freq'] = np.nanmedian(gain_amplitudes, axis = 1).tolist()
+				self.metrics[p][tn]['var_amp_freq'] = np.nanvar(gain_amplitudes, axis = 1).tolist() 
+				self.metrics[p][tn]['rms_amp_freq'] = np.sqrt(np.nanmean(gain_amplitudes ** 2, axis = 1)).tolist()
+			# skewness of the variance across frequency avergaed over short baselines 
+			self.metrics[p]['var_skewness_uvcut'] = self.skewness_across_uvcut(self.metrics['uvcut'])
+	
+	def write_to(self, outfile=None):
+		if outfile is None:
+			outfile = self.calfile.replace('.fits', '_cal_metrics.json')
+		ju.write_metrics(self.metrics, outfile)
