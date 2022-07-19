32a4f0780085455b92019d19ec70b5a69b873732
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-07-19 17:10:08 +0800

    added metrics for bad antennas and link to tables

--- mwa_qa/cal_metrics.py
+++ mwa_qa/cal_metrics.py
@@ -4,7 +4,11 @@ from mwa_qa import json_utils as ju
 from collections import OrderedDict
 from scipy.ndimage import gaussian_filter1d
 from scipy.signal import savgol_filter
+import pandas as pd
 import numpy as np
+import os
+
+pol_dict = {'XX': 0, 'XY': 1, 'YX': 2, 'YY':	3}
 
 
 class CalMetrics(object):
@@ -30,7 +34,7 @@ class CalMetrics(object):
         Returns variance across frequency for the given tile pair
         - antpair : Antenna pair or tuple of antenna numbers e.g (102, 103)
         - norm : boolean, If True returns normalized gains else unormalized
-                                         gains. Default is set to True.
+                                                                                                                                                                                                                                                                         gains. Default is set to True.
         """
         gain_pairs = self.Csoln.gains_for_antpair(antpair, norm=norm)
         return np.nanvar(gain_pairs, axis=1)
@@ -40,9 +44,9 @@ class CalMetrics(object):
         Returns bls shorter than the specified cut and the variances calculated
         across frequency for each of the antenna pair
         - baseline_cut : Baseline cut in metres, will use only baselines
-                                                                         shorter than the given value
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         shorter than the given value
         - norm : boolean, If True returns normalized gains else unormalized
-                                         gains. Default is set to True.
+                                                                                                                                                                                                                                                                         gains. Default is set to True.
         """
         baseline_dict = self.Metafits.baselines_less_than(uv_cut)
         bls = list(baseline_dict.keys())
@@ -58,9 +62,9 @@ class CalMetrics(object):
         variances averaged over baseliness shorter than the given
         uv length
         - uv_cut : Baseline cut in metres, will use only baselines shorter
-                                           than the given value
+                                                                                                                                                                                                                                                                           than the given value
         - norm : boolean, If True returns normalized gains else unormalized
-                                         gains. Default is set to True.
+                                                                                                                                                                                                                                                                         gains. Default is set to True.
         """
         _, variances = self.variance_for_baselines_less_than(uv_cut, norm=norm)
         vmean = np.nanmean(variances, axis=1)
@@ -73,7 +77,7 @@ class CalMetrics(object):
         """
         Returns the receivers connected to the various tiles in the array
         - n : Number of receivers in the array. Optional, enabled if
-                          metafits is not provided. Default is 16.
+                                                                                                                                          metafits is not provided. Default is 16.
         """
         if self.Metafits.metafits is None:
             receivers = list(np.arange(1, n + 1))
@@ -139,10 +143,56 @@ class CalMetrics(object):
             count += len(inds)
         return count / (_sh[0] * _sh[1]) * 100
 
+    def convergence_variance(self):
+        convergence = self.Csoln.data(5)
+        return np.nanvar(self.Csoln.data(5), axis=1)
+
+    def _flag_table(self):
+        _d = [['ANTENNAS', self.metrics['FLAGGED_ANTS']],
+              ['BASELINES', self.metrics['FLAGGED_BLS']],
+              ['CHANNELS', self.metrics['FLAGGED_CHS']],
+              ['CONVERGENCE', self.metrics['NON_CONVERGED_CHS']]
+              ]
+        return pd.DataFrame(data=_d, columns=['FIELD', '%FLAG'])
+
+    def _convegence_table(self):
+        convergence_var = self.metrics['CONVERGENCE_VAR']
+        m_thresh = self.metrics['M_THRESH']
+        _d = []
+        for t in range(self.metrics['NTIMES']):
+            _d.append([convergence_var[t], m_thresh])
+            return pd.DataFrame(data=_d, columns=['VAR', 'M_THRESH'])
+
+    def _variance_antenna_stats(self):
+        var = self.metrics['VAR_AMP_FREQ']
+        mvar = np.nanmean(var, axis=1)
+        stdvar = np.nanstd(var, axis=1)
+        error = var - mvar
+        return error, stdvar
+
+    def variance_antenna_outlier(self, pol, dev=3):
+        error, std = self._variance_antenna_stats()
+        _sh = error.shape
+        pol_ind = pol_dict[pol.upper()]
+        d_error = error[:, :, pol_ind]
+        d_std = std[:, pol_ind]
+        annumbers = self.Metafits.annumbers()
+        poor_antennas = []
+        for t in range(_sh[0]):
+            inds = np.where(d_error[t, :] > dev * d_std[t])[0]
+            outliers = np.array(annumbers)[inds]
+            poor_antennas.append(outliers.tolist())
+            return poor_antennas
+
+    def _variance_table(self):
+        meanvar_amp_freq = np.nanmean(var_amp_freq, axis=1)
+        stdvar_amp_freq = np.nanstd(var_amp_freq, axis=1)
+        errorvar_amp_freq = var_amp_freq - meanvar_amp_freq
+
     def _initialize_metrics_dict(self):
         """
         Initializes the metric dictionary with some of the default
-                                        parameters
+        parameters
         """
         self.metrics = OrderedDict()
         _, freqs, _ = self.Csoln.freqs_info()
@@ -161,10 +211,17 @@ class CalMetrics(object):
         self.metrics['NFREQ'] = len(freqs)
         self.metrics['ANNUMS'] = annumbers
         self.metrics['RECEIVERS'] = receivers.tolist()
+        self.metrics['M_THRESH'] = self.Csoln.header(0)['M_THRESH']
+        self.metrics['S_THRESH'] = self.Csoln.header(0)['S_THRESH']
         self.metrics['CONVERGENCE'] = OrderedDict()
         self.metrics['DELAY_SPECTRUM'] = OrderedDict()
+        self.metrics['POOR_ANTS'] = OrderedDict()
 
-    def run_metrics(self, window_length=19, polyorder=4, sigma=2):
+    def make_clickable(self, val):
+        return f'<a target="_blank" href="{val}">{val}</a>'
+
+    def run_metrics(self, window_length=19, polyorder=4, sigma=2,
+                    html=None, html_link=None):
         self._initialize_metrics_dict()
         pols = self.metrics['POLS']
         receivers = self.metrics['RECEIVERS']
@@ -180,6 +237,7 @@ class CalMetrics(object):
         median_amp_freq = np.nanmedian(gain_amps, axis=2)
         var_amp_freq = np.nanvar(gain_amps, axis=2)
         rms_amp_freq = np.sqrt(np.nanmean(gain_amps ** 2, axis=2))
+
         # skewness across ucvut
         skewness = self.skewness_across_uvcut(self.metrics['UVCUT'])
         # metrics based on antennas connected to receivers
@@ -188,8 +246,7 @@ class CalMetrics(object):
             rcv_gains = self.Csoln.gains_for_receiver(r)
             rcv_amps = np.abs(rcv_gains)
             rcv_amps_mean = np.nanmean(rcv_amps, axis=1)
-            chisq = np.nansum(
-                ((rcv_amps - rcv_amps_mean) / rcv_amps), axis=2)
+            chisq = np.nansum(((rcv_amps - rcv_amps_mean) / rcv_amps), axis=2)
             rcv_chisq[i] = chisq
         # metrics from convergence
         sm_cal_precisions = self.smooth_calibration_precisions(
@@ -202,6 +259,7 @@ class CalMetrics(object):
         self.metrics['FLAGGED_CHS'] = self.flagged_channels_percent()
         self.metrics['FLAGGED_ANTS'] = self.flagged_antennas_percent()
         self.metrics['NON_CONVERGED_CHS'] = self.non_converging_percent()
+        self.metrics['CONVERGENCE_VAR'] = self.convergence_variance()
         self.metrics['MEAN_AMP_ANT'] = mean_amp_ant
         self.metrics['MEDIAN_AMP_ANT'] = median_amp_ant
         self.metrics['VAR_AMP_ANT'] = var_amp_ant
@@ -211,10 +269,33 @@ class CalMetrics(object):
         self.metrics['VAR_AMP_FREQ'] = var_amp_freq
         self.metrics['RMS_AMP_FREQ'] = rms_amp_freq
         self.metrics['RECEIVER_CHISQ'] = rcv_chisq
-        self.metrics['SKEWNESS_UCVUT'] = skewness
+        # self.metrics['SKEWNESS_UCVUT'] = skewness
         self.metrics['CONVERGENCE']['SM_PRECISIONS'] = sm_cal_precisions
         self.metrics['CONVERGENCE']['SM_PRECISIONS_ERROR'] = sm_cal_error
         self.metrics['DELAY_SPECTRUM']['DFFT'] = fft_spectrum
+        self.metrics['POOR_ANTS']['XX'] = self.variance_antenna_outliers('XX')
+        self.metrics['POOR_ANTS']['YY'] = self.variance_antenna_outliers(
+            'YY')
+
+        if html:
+            if html_link is None:
+                html_link = 'cal_metrics.html'
+            self.metrics['HTML'] = html_link
+            # create a HTML table for convergence
+            df1 = self._flag_table()
+            df_html = df1.to_html(render_links=True, escape=False)
+            df2 = self._convegence_table()
+
+            with open(html_link, 'w') as f:
+                f.write('<center>'
+                        + '<h1> {} </h1><br><hr>'.format(self.metrics['OBSID'])
+                        + '<h2> FLAGS </h2>' +
+                        df1.to_html(index=False, border=2,
+                                    justify="center") + '<be><hr>'
+                        + '<h2> CALIBRAITON RESULTS </h2>' +
+                        df2.to_html(index=False, border=2,
+                                    justify="center") + '<br><hr>'
+                        + '</center>')
 
     def write_to(self, outfile=None):
         if outfile is None:
