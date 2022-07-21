8439075bb27aca19cf43de5ddbd922ea0bcd73f3
Ridhima Nunhokee <ridhima@192-168-1-107.tpgi.com.au>
2022-07-21 15:32:11 +0800

    added coord_utils for initialization

--- mwa_qa/__init__.py
+++ mwa_qa/__init__.py
@@ -2,4 +2,6 @@
 __init__.py file from mwa_qa
 """
 
-from mwa_qa import read_csolutions, read_metafits, read_uvfits, cal_metrics, image_metrics, image_utils, vis_metrics, json_utils
+from mwa_qa import read_csolutions, read_metafits, read_uvfits,\
+    cal_metrics, image_metrics, image_utils, vis_metrics, json_utils,\
+    coord_utils
