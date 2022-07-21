60d42284f612ca3783c3894b31d18774598855d7
Ridhima Nunhokee <ridhima@192-168-1-107.tpgi.com.au>
2022-07-21 16:54:26 +0800

    made normalization reference antenna global variables

--- mwa_qa/read_csolutions.py
+++ mwa_qa/read_csolutions.py
@@ -6,31 +6,58 @@ import copy
 
 
 class Csoln(object):
-    def __init__(self, calfile, metafits=None, pol='X'):
+    def __init__(self, calfile, metafits=None, pol='X',
+                 norm=True, ref_antnum=None):
         """
         Object takes in a calfile in fits format and extracts
         bit and pieces of the required informations
         - calfile:	Fits file readable by astropy containing
-                                calibration solutions (support for hyperdrive
-                                output only for now) and related information
+                    calibration solutions (support for hyperdrive
+                    output only for now) and related information
         - metafits:	Metafits with extension *.metafits containing
-                                information corresponding to the observation
-                                for which the calibration solutions is derived
+                    information corresponding to the observation
+                    for which the calibration solutions is derived
         - pol:	Polarization, can be either 'X' or 'Y'. It should be
                         specified so that information associated
                         with the given pol is provided. Default is 'X'
+        - norm: Boolean, If True, the calibration solutions will be
+                normlaized else unnormlaized solutions will be used.
+                Default is set to True
+        - ref_antnum:   Reference antenna number. If norm is True,
+                        a reference antenna is require for normalization.
+                        By default it uses the last antenna in the array.
+                        If the last antenna is flagged, it will return
+                        an error.
         """
         self.calfile = calfile
         self.Metafits = rm.Metafits(metafits, pol)
+        self.norm = norm
+        if self.norm:
+            if ref_antnum is None:
+                annumbers, _, _ = self.ant_info()
+                ref_antnum = annumbers[-1]
+                self.ref_antnum = ref_antnum
+                self._check_refant()
+
+    def _check_refant(self):
+        """
+        Checks if the given reference antenna is flagged due to non-convergence
+        or any malfunctioning reports
+        """
+        annumbers, annames, anflags = self.ant_info()
+        ind = self.gains_ind_for(self.ref_antnum)
+        flag = np.array(anflags)[ind]
+        assert flag == 0,  "{} seems to be flagged."
+        "calibration solutions found, choose a different tile"
 
     def data(self, hdu):
         """
         Returns the data stored in the specified HDU column of the image
         hdu:	HDU column, ranges from 1 to 6
-                                1 - the calibration solution
-                                2 - the start time, end time and average time
-                                3 - tiles information (antenna, tilename, flag)
-                                4 - chanblocks (index, freq, flag)
+                1 - the calibration solution
+                2 - the start time, end time and average time
+                3 - tiles information (antenna, tilename, flag)
+                4 - chanblocks (index, freq, flag)
                 5 - calibration results (timeblock, chan, convergence)
                 6 - weights used for each baseline
         For more details refer to
@@ -43,7 +70,7 @@ class Csoln(object):
         Returns the header of the specified HDU column
         hdu:	HDU column, ranges from 0 to 6
                 0 - header information on the paramters used for the
-                                        calibration process
+                    calibration process
                 1 - header information on the calibration solutions
                 2 - header information on the timeblocks
                 3 - header information on the tiles (antenna, tilename, flag)
@@ -121,35 +148,18 @@ class Csoln(object):
         """
         return antnum
 
-    def _check_refant(self, antnum):
-        """
-        Checks if the given reference antenna is flagged due to non-convergence
-                or any malfunctioning reports
-        - antnum : Antenna Number, starts from 0
-        """
-        annumbers, annames, anflags = self.ant_info()
-        ind = self.gains_ind_for(antnum)
-        flag = np.array(anflags)[ind]
-        assert flag == 0,  "{} seems to be flagged."
-        "calibration solutions found, choose a different tile"
-
-    def _normalized_data(self, data, ref_antnum=None):
+    def _normalized_data(self, data):
         """
         Normalizes the gain solutions for each timeblock given a reference tile
         - data:	Input array of shape( tiles, freq, pols) containing the
-                                solutions
-        - ref_antnum:	Antenna number for the reference antenna (starts from 1).
-                        Default is set to the last antenna of the telescope.
-                        For example for MWA128T, the reference antenna is
-                        Antenna 128
+                solutions
         """
-        if ref_antnum is None:
-            annumbers, annames, _ = self.ant_info()
-            ref_ind = -1
-            ref_antnum = np.array(annumbers)[ref_ind]
-        else:
-            ref_ind = self.gains_ind_for(ref_antnum)
-        self._check_refant(ref_antnum)
+        # if self.ref_antnum is None:
+        #    annumbers, annames, _ = self.ant_info()
+        #    ref_ind = -1
+        #    ref_antnum = np.array(annumbers)[ref_ind]
+        # else:
+        ref_ind = self.gains_ind_for(self.ref_antnum)
         refs = []
         for ref in data[ref_ind].reshape((-1, 2, 2)):
             refs.append(np.linalg.inv(ref))
@@ -160,53 +170,49 @@ class Csoln(object):
                 div_ref.append(i.reshape((2, 2)).dot(ref))
         return np.array(div_ref).reshape(data.shape)
 
-    def normalized_gains(self, ref_antnum=None):
+    def normalized_gains(self):
         """
         Returns the normalized gain solutions using the
         given reference Antenna number
-        - ref_antnum:	Antenna number for the reference antenna (starts from 1).
-                        Default is set to the last antenna of the telescope.
-                        For example for MWA128T, the reference antenna is
-                        Antenna 128
         """
         gains = self.gains()
         ngains = copy.deepcopy(gains)
         for t in range(len(ngains)):
-            ngains[t] = self._normalized_data(gains[t], ref_antnum)
+            ngains[t] = self._normalized_data(gains[t])
         return ngains
 
-    def _select_gains(self, norm):
+    def _select_gains(self):
         """
         Return normalized if norm is True else unnomalized gains
         - norm:	Boolean, If True returns normalized gains else
                 unormalized gains.
         """
-        if norm:
+        if self.norm:
             return self.normalized_gains()
         else:
             return self.gains()
 
-    def amplitudes(self, norm=True):
+    def amplitudes(self):
         """
         Returns amplitude of the normalized gain solutions
         - norm:	Boolean, if True returns normalized gains else
                 unormalized gains.
                 Default is set to True.
         """
-        gains = self._select_gains(norm=norm)
+        gains = self._select_gains()
         return np.abs(gains)
 
-    def phases(self, norm=True):
+    def phases(self):
         """
         Returns phases in degrees of the normalized gain solutions
         - norm: Boolean, if True returns normalized gains else
                 unormalized gains.
                 Default is set to True.
         """
-        gains = self._select_gains(norm=norm)
+        gains = self._select_gains()
         return np.angle(gains) * 180 / np.pi
 
-    def gains_for_antnum(self, antnum, norm=True):
+    def gains_for_antnum(self, antnum):
         """
         Returns gain solutions for the given tile ID
         - antnum:	Antenna Number, starts from 1
@@ -214,21 +220,21 @@ class Csoln(object):
                 else unormalized gains.
                 Default is set to True.
         """
-        gains = self._select_gains(norm=norm)
+        gains = self._select_gains()
         ind = self.gains_ind_for(antnum)
         return gains[:, ind, :, :]
 
-    def gains_for_antpair(self, antpair, norm=True):
+    def gains_for_antpair(self, antpair):
         """
         Evaluates conjugation of the gain solutions for antenna pair
                 (tile0, tile1)
         - antpair:	Tuple of antenna numbers such as (1, 2)
         """
-        gains_t0 = self.gains_for_antnum(antpair[0], norm=norm)
-        gains_t1 = self.gains_for_antnum(antpair[1], norm=norm)
+        gains_t0 = self.gains_for_antnum(antpair[0])
+        gains_t1 = self.gains_for_antnum(antpair[1])
         return gains_t0 * np.conj(gains_t1)
 
-    def gains_for_receiver(self, receiver, norm=True):
+    def gains_for_receiver(self, receiver):
         """
         Returns the dictionary of gains solutions for all the antennas
                 (8 antennas in principles) connected to the given receiver
