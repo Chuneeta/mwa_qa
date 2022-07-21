40a390df358b035cf85cd0e89965daa4fb2c05e4
Ridhima Nunhokee <ridhima@192-168-1-107.tpgi.com.au>
2022-07-21 14:25:13 +0800

    reorganized metrics

--- mwa_qa/vis_metrics.py
+++ mwa_qa/vis_metrics.py
@@ -3,6 +3,8 @@ from mwa_qa import json_utils as ju
 from collections import OrderedDict
 import numpy as np
 
+pol_dict = ru.pol_dict
+
 
 class VisMetrics(object):
     def __init__(self, uvfits):
@@ -35,37 +37,44 @@ class VisMetrics(object):
         self.metrics['NTIMES'] = self.uvf.Ntimes
         self.metrics['NFREQS'] = self.uvf.Nfreqs
         self.metrics['NPOLS'] = self.uvf.Npols
-        self.metrics['DATA'] = OrderedDict(
-            [('AUTOS', OrderedDict()), ('CROSS', OrderedDict())])
+        self.metrics['AUTOS'] = OrderedDict(
+            [('XX', OrderedDict()), ('YY', OrderedDict())])
 
-    def run_metrics(self):
+    def run_metrics(self, dev=3):
         self._initialize_metrics_dict()
         self.metrics['NANTS']
         self.metrics['NFREQS']
         self.metrics['NPOLS']
         autos = self.autos()
-        # averaged over time
-        avg_autos = np.nanmean(autos, axis=0)
-        amps_avg_autos = np.abs(avg_autos)
-        # metrics across antennas
-        mean_amp_ant = np.nanmean(amps_avg_autos, axis=0)
-        median_amp_ant = np.nanmedian(amps_avg_autos, axis=0)
-        var_amp_ant = np.nanvar(amps_avg_autos, axis=0)
-        rms_amp_ant = np.sqrt(np.nanmean(amps_avg_autos ** 2, axis=0))
         # metrics across frequency
-        mean_amp_freq = np.nanmean(amps_avg_autos, axis=1)
-        median_amp_freq = np.nanmedian(amps_avg_autos, axis=1)
-        var_amp_freq = np.nanvar(amps_avg_autos, axis=1)
-        rms_amp_freq = np.sqrt(np.nanmean(amps_avg_autos ** 2, axis=1))
-
-        self.metrics['MEAN_AMP_ANT'] = mean_amp_ant
-        self.metrics['MEDIAN_AMP_ANT'] = median_amp_ant
-        self.metrics['VAR_AMP_ANT'] = var_amp_ant
-        self.metrics['RMS_AMP_ANT'] = rms_amp_ant
-        self.metrics['MEAN_AMP_FREQ'] = mean_amp_freq
-        self.metrics['MEDIAN_AMP_FREQ'] = median_amp_freq
-        self.metrics['VAR_AMP_FREQ'] = var_amp_freq
-        self.metrics['RMS_AMP_FREQ'] = rms_amp_freq
+        used_pols = ['XX', 'YY']
+        for i, p in enumerate(used_pols):
+            avg_autos = autos[:, :, :, pol_dict[p]]
+            amps_avg_autos = np.abs(avg_autos)
+            var_amp_freq = np.nanvar(amps_avg_autos, axis=2)
+            rms_amp_freq = np.sqrt(np.nanmean(amps_avg_autos ** 2, axis=2))
+            mrms_amp_freq = np.nanmean(rms_amp_freq, axis=0)
+            mstd_amp_freq = np.nanstd(rms_amp_freq, axis=0)
+            min_lim = mrms_amp_freq - dev * mstd_amp_freq
+            max_lim = mrms_amp_freq + dev * mstd_amp_freq
+            # picking poor behaving timestamps
+            poor_times = []
+            for i in range(self.metrics['NANTS']):
+                inds_t = np.where((rms_amp_freq[:, i] < min_lim[i]) | (
+                    rms_amp_freq[:, i] > max_lim[i]))[0]
+            poor_times.append(inds_t.tolist())
+            median_amp_time = np.nanmedian(amps_avg_autos, axis=0)
+            rms_amp_time = np.sqrt(np.nanmean(amps_avg_autos ** 2, axis=0))
+            median_amp_ant = np.nanmedian(amps_avg_autos, axis=1)
+            rms_amp_ant = np.sqrt(np.nanmean(amps_avg_autos ** 2, axis=1))
+            # writing to json files
+            self.metrics['AUTOS'][p]['POOR_TIMES'] = inds_t
+            self.metrics['AUTOS'][p]['MEDIAN_AMP_TIME'] = median_amp_time
+            self.metrics['AUTOS'][p]['RMS_AMP_TIME'] = rms_amp_time
+            self.metrics['AUTOS'][p]['MEDIAN_AMP_ANT'] = median_amp_ant
+            self.metrics['AUTOS'][p]['RMS_AMP_ANT'] = rms_amp_ant
+            self.metrics['AUTOS'][p]['VAR_AMP_FREQ'] = var_amp_freq
+            self.metrics['AUTOS'][p]['RMS_AMP_FREQ'] = rms_amp_freq
 
     def write_to(self, outfile=None):
         if outfile is None:
