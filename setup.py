7fe8a6c60b7b8c42b57dd41129d6545c3d453d15
Ridhima Nunhokee <ridhima@192-168-1-107.tpgi.com.au>
2022-07-21 15:37:22 +0800

    added other modules needed

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 from mwa_qa import version
-import sys
 import os
 import json
 
-data = [version.git_origin, version.git_hash, version.git_description, version.git_branch]
+data = [version.git_origin, version.git_hash,
+        version.git_description, version.git_branch]
 with open(os.path.join('mwa_qa', 'GIT_INFO'), 'w') as outfile:
     json.dump(data, outfile)
+
 
 def package_files(package_dir, subdirectory):
     # walk the input package_dir/subdirectory
@@ -18,7 +19,9 @@ def package_files(package_dir, subdirectory):
             path = path.replace(package_dir + '/', '')
             paths.append(os.path.join(path, filename))
     return paths
-data_files = package_files('mwa_qa', 'data') 
+
+
+data_files = package_files('mwa_qa', 'data')
 
 setup_args = {
     'name':         'mwa_qa',
@@ -30,10 +33,12 @@ setup_args = {
     'packages':     ['mwa_qa'],
     'package_dir':  {'mwa_qa': 'mwa_qa'},
     'package_data': {'mwa_qa': data_files},
-    'install_requires': ['numpy>=1.14', 'scipy', 'astropy>3.0.0', 'matplotlib>=2.2', 'python_dateutil>=2.6.0', 'pytest'],
+    'install_requires': ['numpy>=1.14', 'scipy', 'astropy>3.0.0',
+                         'matplotlib>=2.2', 'python_dateutil>=2.6.0',
+                         'pytest', 'itertools'],
     'include_package_data': True,
     'zip_safe':     False,
 }
 
 if __name__ == '__main__':
-    setup(*(), **setup_args)   
+    setup(*(), **setup_args)
