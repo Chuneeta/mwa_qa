c1c947222fbdcb7397ba6606ab62fab76f583b35
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-07-26 12:43:28 +0800

    added command line scripts

---
+++ scripts/run_calqa.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser
+from pathlib import Path
+from mwa_qa.cal_metrics import CalMetrics
+
+parser = ArgumentParser(
+    description="QA for hyperdrive calibration solutions")
+parser.add_argument('soln', type=Path, help='Hyperdrive fits file')
+parser.add_argument('metafits', type=Path, help='MWA metadata fits file')
+parser.add_argument('pol', default=None,
+                    help='Polarization, can be either X or Y')
+parser.add_argument(
+    '--out', help='json output path', type=Path, default=None, required=False
+)
+
+args = parser.parse_args()
+m = CalMetrics(str(args.soln), str(args.metafits), args.pol)
+m.run_metrics()
+m.write_to(str(args.out))
