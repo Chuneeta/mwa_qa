560a9c815eea5ebbc08119923c89d16e93103986
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-08-08 16:15:46 +0800

    removed unused scripts

--- mwa_qa/__init__.py
+++ mwa_qa/__init__.py
@@ -2,5 +2,5 @@
 __init__.py file from mwa_qa
 """
 
-from mwa_qa import read_csolutions, read_image, read_metafits, cal_metrics, image_metrics, read_uvfits1, vis_metrics, json_utils,\
+from mwa_qa import read_csolutions, read_image, read_metafits, cal_metrics, image_metrics, vis_metrics, json_utils,\
     coord_utils, read_uvfits
