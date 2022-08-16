4aa32693e1edc1bc70bba5ea3cad62d72c7fa4c1
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-08-16 17:09:05 +0800

    initiated read_calqa

--- mwa_qa/__init__.py
+++ mwa_qa/__init__.py
@@ -2,5 +2,5 @@
 __init__.py file from mwa_qa
 """
 
-from mwa_qa import read_csolutions, read_image, read_metafits, cal_metrics, image_metrics, vis_metrics, json_utils,\
-    coord_utils, read_uvfits
+from mwa_qa import read_csolutions, read_image, read_metafits, cal_metrics, \
+    image_metrics, vis_metrics, json_utils, coord_utils, read_uvfits, read_calqa
