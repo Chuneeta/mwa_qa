9a9115ee5bcce66faac53a87c0b7c55438878cd7
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-08-18 17:09:13 +0800

    accounted for out of bound pixels

--- mwa_qa/read_image.py
+++ mwa_qa/read_image.py
@@ -53,18 +53,28 @@ class Image(object):
         ra, dec = src_pos
         w = wcs.WCS(self.fitspath)
         xpix, ypix = w.all_world2pix(ra, dec, 1, 0, 0)[:2]
-        return np.around(xpix, 0).astype(int), np.around(ypix, 0).astype(int)
+        if (xpix > 0) and (xpix < self.image_size[0]):
+            if (ypix > 0) and (ypix < self.image_size[0]):
+                xpix, ypix = np.around(xpix, 0).astype(
+                    int), np.around(ypix, 0).astype(int)
+        else:
+            xpix, ypix = np.nan, np.nan
+
+        return xpix, ypix
 
     def src_flux(self, src_pos, beam_const=1, deconvol=False):
         if deconvol:
             pflux, tflux, std = self.fit_gaussian(src_pos, beam_const)
         else:
             select = self._select_region(src_pos, beam_const)
-            region = self.data_array.squeeze()[select]
-            pflux = np.nanmax(region)
-            # might have negative pixels as well
-            tflux = np.nansum(np.abs(region)) / self.beam_npix
-            std = np.nanstd(region)
+            if select is None:
+                pflux, tflux, std = np.nan, np.nan, np.nan
+            else:
+                region = self.data_array.squeeze()[select]
+                pflux = np.nanmax(region)
+                # might have negative pixels as well
+                tflux = np.nansum(np.abs(region)) / self.beam_npix
+                std = np.nanstd(region)
 
         return pflux, tflux, std
 
@@ -72,43 +82,51 @@ class Image(object):
         bm_radius_px = np.sqrt(self.beam_major_px **
                                2 + self.beam_minor_px ** 2)
         ra_pix, dec_pix = self.src_pix(src_pos)
-        l_axis = np.arange(self.image_size[0])
-        m_axis = np.arange(self.image_size[1])
-        ll, mm = np.meshgrid(l_axis, m_axis)
-        R = np.sqrt((ll - ra_pix)**2 + (mm - dec_pix)**2)
-        select = R < beam_const * bm_radius_px
+        if np.isnan(ra_pix) or np.isnan(dec_pix):
+            warnings.warn('Source position out of bounds')
+            select = None
+        else:
+            l_axis = np.arange(self.image_size[0])
+            m_axis = np.arange(self.image_size[1])
+            ll, mm = np.meshgrid(l_axis, m_axis)
+            R = np.sqrt((ll - ra_pix)**2 + (mm - dec_pix)**2)
+            select = R < beam_const * bm_radius_px
+
         return select
 
     def fit_gaussian(self, src_pos, bm_const):
         select = self._select_region(src_pos, bm_const)
-        l_axis = np.arange(self.image_size[0])
-        m_axis = np.arange(self.image_size[1])
-        ll, mm = np.meshgrid(l_axis, m_axis)
-        gauss_data = self.data_array.squeeze()
-        gauss_data[~select] = 0
-        gauss_data = gauss_data.reshape(self.image_size)
-        peak_val = np.nanmax(gauss_data)
-        inds = np.where(gauss_data == peak_val)
-        mod = models.Gaussian2D(peak_val, inds[1], inds[0],
-                                self.beam_major_px / 2, self.beam_minor_px / 2,
-                                theta=self.beam_parallactic_angle * np.pi/180)
-        fit_p = fitting.LevMarLSQFitter()
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore')
-        try:
+        if select is None:
+            gauss_pflux, gauss_tflux, gauss_std = np.nan, np.nan, np.nan
+        else:
+            l_axis = np.arange(self.image_size[0])
+            m_axis = np.arange(self.image_size[1])
+            ll, mm = np.meshgrid(l_axis, m_axis)
+            gauss_data = self.data_array.squeeze()
+            gauss_data[~select] = 0
+            gauss_data = gauss_data.reshape(self.image_size)
+            peak_val = np.nanmax(gauss_data)
+            inds = np.where(gauss_data == peak_val)
             mod = models.Gaussian2D(peak_val, inds[1], inds[0],
-                                    self.beam_major_px / 2,
-                                    self.beam_minor_px / 2,
-                                    theta=self.beam_parallactic_angle
-                                    * np.pi/180)
-            gauss_mod = fit_p(mod, ll, mm, gauss_data)
-        except ValueError:
-            mod = models.Gaussian2D(peak_val, inds[0][1], inds[0][0],
-                                    self.beam_major_px / 2,
-                                    self.beam_minor_px / 2,
-                                    theta=self.parallactic_angle * np.pi/180)
-            gauss_mod = fit_p(mod, ll, mm, gauss_data)
-        gauss_pflux = gauss_mod.amplitude.value
-        gauss_tflux = np.nansum(gauss_mod(ll, mm)) / self.beam_npix
-        gauss_std = np.nanstd(gauss_mod(ll, mm))
+                                    self.beam_major_px / 2, self.beam_minor_px / 2,
+                                    theta=self.beam_parallactic_angle * np.pi/180)
+            fit_p = fitting.LevMarLSQFitter()
+            with warnings.catch_warnings():
+                warnings.simplefilter('ignore')
+            try:
+                mod = models.Gaussian2D(peak_val, inds[1], inds[0],
+                                        self.beam_major_px / 2,
+                                        self.beam_minor_px / 2,
+                                        theta=self.beam_parallactic_angle
+                                        * np.pi/180)
+                gauss_mod = fit_p(mod, ll, mm, gauss_data)
+            except ValueError:
+                mod = models.Gaussian2D(peak_val, inds[0][1], inds[0][0],
+                                        self.beam_major_px / 2,
+                                        self.beam_minor_px / 2,
+                                        theta=self.parallactic_angle * np.pi/180)
+                gauss_mod = fit_p(mod, ll, mm, gauss_data)
+            gauss_pflux = gauss_mod.amplitude.value
+            gauss_tflux = np.nansum(gauss_mod(ll, mm)) / self.beam_npix
+            gauss_std = np.nanstd(gauss_mod(ll, mm))
         return gauss_pflux, gauss_tflux, gauss_std
