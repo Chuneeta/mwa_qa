1b58fd50339e8e06fd946ff224f487ad3763d051
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-08-19 13:33:32 +0800

    added beam constant

--- scripts/run_imgqa.py
+++ scripts/run_imgqa.py
@@ -6,7 +6,7 @@ from mwa_qa.image_metrics import ImgMetrics
 parser = ArgumentParser(description="QA for MWA images")
 parser.add_argument('fits', type=Path, nargs='+',
                     help='FITS image file(s)')
-parser.add_argument('--const', type=int,
+parser.add_argument('--const', type=int, default=1,
                     help='Beam contant to multiply for the beam radius for region selection')
 parser.add_argument('--out', help='json output path',
                     type=str, default=None, required=False)
