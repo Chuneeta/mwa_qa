04cd7296ea6ea34b873d98f8e165bc92a48f34d3
Ridhima Nunhokee <ridhima@192-168-1-107.tpgi.com.au>
2022-08-26 06:44:28 +0800

    added unittests

--- mwa_qa/tests/test_cal_metrics.py
+++ mwa_qa/tests/test_cal_metrics.py
@@ -1,7 +1,8 @@
 from collections import OrderedDict
-from mwa_qa import read_csolutions as rc
-from mwa_qa import read_metafits as rm
-from mwa_qa import cal_metrics as cm
+from mwa_qa.read_calfits import CalFits
+from mwa_qa.read_metafits import Metafits
+from mwa_qa.cal_metrics import CalMetrics
+from astropy.io import fits
 from mwa_qa.data import DATA_PATH
 import unittest
 import numpy as np
@@ -9,71 +10,180 @@ import os
 
 calfile = os.path.join(DATA_PATH, 'test_1061315688.fits')
 metafits = os.path.join(DATA_PATH, 'test_1061315688.metafits')
+hdu = fits.open(calfile)
+exp_gains = hdu[1].data[:, :, :, ::2] + hdu[1].data[:, :, :, 1::2] * 1j
+_sh = hdu[1].data.shape
 
 
 class TestCalMetrics(unittest.TestCase):
     def test_init__(self):
-        m = cm.CalMetrics(calfile, metafits=metafits)
-        self.assertEqual(m.calfile, calfile)
-        self.assertEqual(m.Csoln.calfile, calfile)
-        self.assertEqual(m.Csoln.Metafits.metafits, metafits)
-        self.assertEqual(m. Csoln.Metafits.pol, 'X')
+        m = CalMetrics(calfile, metafits_path=metafits)
+        self.assertEqual(m.calfits_path, calfile)
+        self.assertEqual(m.CalFits.calfits_path, calfile)
+        self.assertEqual(m.CalFits.Metafits.metafits, metafits)
+        self.assertEqual(m.CalFits.Metafits.pol, 'X')
+        self.assertEqual(m.Metafits.metafits, metafits)
+        self.assertEqual(m.Metafits.pol, 'X')
+        self.assertEqual(m.CalFits.gain_array.shape, (1, 128, 768, 4))
+        self.assertEqual(m.CalFits.start_time, hdu['TIMEBLOCKS'].data[0][0])
+        self.assertEqual(m.CalFits.end_time, hdu['TIMEBLOCKS'].data[0][1])
+        self.assertEqual(m.CalFits.average_time, hdu['TIMEBLOCKS'].data[0][2])
+        np.testing.assert_equal(np.array(m.CalFits.antenna), np.arange(128))
+        expected_flags = np.zeros((128))
+        expected_flags[76] = 1
+        np.testing.assert_equal(
+            np.array(m.CalFits.antenna_flags), expected_flags)
+        expected_annames = [tl[1] for tl in hdu[3].data]
+        np.testing.assert_equal(np.array(m.CalFits.tilename), expected_annames)
+        np.testing.assert_almost_equal(
+            m.CalFits.frequency_channels, np.arange(0, 768))
+        self.assertEqual(m.CalFits.frequency_array[0], 167055000.0)
+        self.assertEqual(m.CalFits.frequency_array[-1], 197735000.0)
+        inds = np.where(np.array(m.CalFits.frequency_flags) == 0)
+        self.assertEqual(len(inds[0]), 648)
+        inds = np.where(np.array(m.CalFits.frequency_flags) == 1)
+        self.assertEqual(len(inds[0]), 768 - 648)
+        np.testing.assert_almost_equal(
+            m.CalFits.gain_array[0, 0, 100, :], exp_gains[0, 0, 100, :])
+        np.testing.assert_almost_equal(m.CalFits.amplitudes[0, 0, 100, :], np.array(
+            [0.76390112, 0.02917631, 0.0354274, 0.84958042]))
+        np.testing.assert_almost_equal(m.CalFits.phases[0, 0, 100, :], np.array(
+            [1.58846751, 1.90056897, 0.87910414, 1.23280019]))
+        np.testing.assert_almost_equal(
+            m.CalFits.convergence, hdu['RESULTS'].data)
+        np.testing.assert_almost_equal(
+            m.CalFits.baseline_weights, hdu['BASELINES'].data)
+        self.assertTrue(m.CalFits.norm == False)
+        m = CalMetrics(calfile, metafits_path=metafits, norm=True)
+        self.assertTrue(m.CalFits.norm == True)
+        self.assertTrue(m.CalFits.ref_antenna == 127)
+        np.testing.assert_almost_equal(
+            m.CalFits.gain_array[0, 0, 100, :], np.array([
+                0.71117848-0.70117164j, -0.03601556-0.02672433j,
+                0.03182042+0.02749634j, -0.72570327-1.00192601j]))
+        np.testing.assert_almost_equal(m.CalFits.amplitudes[0, 0, 100, :], np.array(
+            [0.99870742, 0.04484763, 0.04205458, 1.23713418]))
+        np.testing.assert_almost_equal(m.CalFits.phases[0, 0, 100, :], np.array(
+            [-0.77831304, -2.5032171,  0.71262886, -2.19765094]))
 
-    # def test_variance_for_antpair(self):
-    #    m = cm.CalMetrics(calfile, metafits=metafits)
-    #    variance = m.variance_for_antpair((102, 104))
-    #    self.assertEqual(variance.shape, (1, 1, 4))
-    #    np.testing.assert_almost_equal(variance[0, 0, :], np.array(
-    #        [6.15063689e-02, 6.10921185e-06, 1.24056403e-05, 8.76188245e-02]), decimal=6)
+    def test_variance_for_antpair(self):
+        m = CalMetrics(calfile)
+        variance = m.variance_for_antpair((0, 0))
+        self.assertEqual(variance.shape, (1, 4))
+        np.testing.assert_almost_equal(variance[0, :], np.array(
+            [9.39389297e-03, 1.16491587e-06, 1.94889201e-06, 7.67858028e-03]), decimal=6)
 
-    # def test_variance_for_baselines_less_than(self):
-    #    m = cm.CalMetrics(calfile, metafits=metafits)
-    #    bls, variances = m.variance_for_baselines_less_than(10)
-    #   self.assertEqual(len(bls), 23)
-    #    self.assertEqual(bls, [(23, 21), (22, 21), (27, 26), (27, 43), (27, 41), (26, 25), (25, 43), (14, 36), (13, 12), (17, 16), (
-    #        17, 15), (94, 47), (84, 83), (38, 37), (37, 42), (44, 45), (43, 41), (42, 41), (42, 45), (47, 45), (64, 63), (64, 66), (64, 65)])
-     #   self.assertEqual(variances.shape, (1, len(bls), 4))
-     #   np.testing.assert_almost_equal(variances[0, 0, :], np.array(
-     #       [1.69654699e-02, 8.44322446e-06, 1.13070715e-05, 2.04571904e-02]), decimal=6)
+    def test_variance_for_baselines_less_than(self):
+        m = CalMetrics(calfile, metafits_path=metafits)
+        variances = m.variance_for_baselines_less_than(10)
+        self.assertEqual(variances.shape, (1, 23, 4))
+        np.testing.assert_almost_equal(variances[0, 0, :], np.array(
+            [8.46222442e-03, 8.99869563e-07, 1.50672493e-06, 8.14717749e-03]), decimal=6)
 
-    # def test_skewness_across_baselines(self):
-     #   m = cm.CalMetrics(calfile, metafits=metafits)
-     #   skewness = m.skewness_across_baselines(10)
-     #   np.testing.assert_almost_equal(skewness, np.array(
-      #      [[1.39739935, 1.1925527, 0.08251331, 1.06225206]]))
+    def test_skewness_across_uvcut(self):
+        m = CalMetrics(calfile, metafits_path=metafits)
+        skewness = m.skewness_across_uvcut(10)
+        np.testing.assert_almost_equal(skewness, np.array(
+            [[0.19086073, 1.24464852, 1.05598102, 0.56893005]]))
 
-    # def test_get_receivers(self):
-    #	m = cm.CalMetrics(calfile, metafits = metafits)
-    #	receivers = m.get_receivers()
-    #	expected = [10,10,10,10,10,10,10,10,7,7,7,7,7,7,7,7,16,16,16,16,16,16,16,16,15,15,15,15,15,15,15,15,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,12,12,12,12,12,12,12,12,11,11,11,11,11,11,11,11,14,14,14,14,14,14,14,14,13,13,13,13,13,13,13,13,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5]
-    #	self.assertEqual(receivers, expected)
-    #	m = cm.CalMetrics(calfile)
-    #	receivers = m.get_receivers()
-    #	self.assertEqual(receivers, list(np.arange(1, 17)))
+    def test_get_receivers(self):
+        m = CalMetrics(calfile, metafits_path=metafits)
+        receivers = m.get_receivers()
+        expected = [10, 10, 10, 10, 10, 10, 10, 10, 7, 7, 7, 7, 7, 7, 7, 7, 16, 16, 16, 16, 16, 16, 16, 16, 15, 15, 15, 15, 15, 15, 15, 15, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9, 9, 9, 8, 8, 8, 8, 8, 8, 8, 8,
+                    12, 12, 12, 12, 12, 12, 12, 12, 11, 11, 11, 11, 11, 11, 11, 11, 14, 14, 14, 14, 14, 14, 14, 14, 13, 13, 13, 13, 13, 13, 13, 13, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 6, 6, 6, 6, 6, 6, 6, 6, 5, 5, 5, 5, 5, 5, 5, 5]
+        self.assertEqual(receivers, expected)
+        m = CalMetrics(calfile)
+        receivers = m.get_receivers()
+        self.assertEqual(receivers, list(np.arange(1, 17)))
 
-    def test_flagged_baselines_percent(self):
-        m = cm.CalMetrics(calfile)
-        percent = m.flagged_baselines_percent()
-        self.assertEqual(percent, 1.5625)
+    def test_unused_baselines_percent(self):
+        m = CalMetrics(calfile)
+        percent = m.unused_baselines_percent()
+        self.assertEqual(percent, 13.065944881889763)
 
-    def test_flagged_channels_percent(self):
-        m = cm.CalMetrics(calfile)
-        percent = m.flagged_channels_percent()
+    def test_unused_channels_percent(self):
+        m = CalMetrics(calfile)
+        percent = m.unused_channels_percent()
         self.assertEqual(percent, 15.625)
 
+    def test_unused_antenna_percent(self):
+        m = CalMetrics(calfile)
+        percent = m.unused_antennas_percent()
+        self.assertEqual(percent, 0.78125)
+
     def test_non_converging_percent(self):
-        m = cm.CalMetrics(calfile)
+        m = CalMetrics(calfile)
         percent = m.non_converging_percent()
         self.assertEqual(percent, 15.625)
 
-    # def test_initialize_metrics_dict(self):
-    #    m = cm.CalMetrics(calfile)
-    #    metrics = m._initialize_metrics_dict()
-    #    self.assertTrue(isinstance(metrics, OrderedDict))
-        #self.assertEqual(list(metrics.keys()), ['pols', 'freqs', 'tile_ids'])
+    def test_convergence_variance(self):
+        m = CalMetrics(calfile)
+        conv_variance = m.convergence_variance()
+        np.testing.assert_almost_equal(
+            conv_variance, np.array([4.93491705e-14]))
+
+    def test_initialize_metrics_dict(self):
+        m = CalMetrics(calfile)
+        m._initialize_metrics_dict()
+        self.assertTrue(isinstance(m.metrics, OrderedDict))
+        self.assertEqual(list(m.metrics.keys()), [
+            'POLS', 'OBSID', 'UVCUT', 'NTIME',
+            'START_FREQ', 'CH_WIDTH', 'ANTENNA',
+            'RECEIVERS', 'XX', 'YY'])
 
     def test_run_metrics(self):
-        pass
+        m = CalMetrics(calfile, metafits)
+        m.run_metrics()
+        self.assertEqual(list(m.metrics.keys()), ['POLS', 'OBSID', 'UVCUT', 'NTIME',
+                                                  'START_FREQ', 'CH_WIDTH', 'ANTENNA', 'RECEIVERS', 'XX', 'YY', 'UNUSED_BLS',
+                                                  'UNUSED_CHS', 'UNUSED_ANTS', 'NON_CONVERGED_CHS', 'CONVERGENCE_VAR', 'RECEIVER_CHISQ'])
+        self.assertEqual(m.metrics['POLS'], ['XX', 'XY', 'YX', 'YY'])
+        self.assertEqual(m.metrics['OBSID'], m.CalFits.obsid)
+        self.assertEqual(m.metrics['UVCUT'], m.CalFits.uvcut)
+        self.assertEqual(m.metrics['NTIME'], m.CalFits.Ntime)
+        self.assertEqual(m.metrics['START_FREQ'], m.CalFits.frequency_array[0])
+        self.assertEqual(m.metrics['CH_WIDTH'], m.CalFits.frequency_array[1]
+                         - m.CalFits.frequency_array[0])
+        self.assertEqual(m.metrics['ANTENNA'], m.CalFits.antenna)
+        self.assertEqual(m.metrics['RECEIVERS'], [
+                         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+        self.assertTrue(isinstance(m.metrics['XX'], OrderedDict))
+        self.assertTrue(isinstance(m.metrics['YY'], OrderedDict))
+        self.assertEqual(m.metrics['UNUSED_BLS'], 13.065944881889763)
+        self.assertEqual(m.metrics['UNUSED_CHS'], 15.625)
+        self.assertEqual(m.metrics['UNUSED_ANTS'], 0.78125)
+        self.assertEqual(m.metrics['NON_CONVERGED_CHS'], 15.625)
+        np.testing.assert_almost_equal(
+            m.metrics['CONVERGENCE_VAR'], np.array([4.93491705e-14]))
+        self.assertEqual(list(m.metrics['XX'].keys()), ['SKEWNESS_UVCUT', 'AMPVAR_ANT', 'AMPRMS_ANT', 'RMS_AMPVAR_ANT',
+                         'AMPVAR_FREQ', 'AMPRMS_FREQ', 'RMS_AMPVAR_FREQ', 'DFFT', 'DFFT_POWER', 'DFFT_POWER_HIGH_PKPL',
+                                                        'DFFT_POWER_HIGH_NKPL', 'RECEIVER_CHISQVAR'])
+        self.assertEqual(list(m.metrics['YY'].keys()), ['SKEWNESS_UVCUT', 'AMPVAR_ANT', 'AMPRMS_ANT', 'RMS_AMPVAR_ANT',
+                         'AMPVAR_FREQ', 'AMPRMS_FREQ', 'RMS_AMPVAR_FREQ', 'DFFT', 'DFFT_POWER', 'DFFT_POWER_HIGH_PKPL',
+                                                        'DFFT_POWER_HIGH_NKPL', 'RECEIVER_CHISQVAR'])
+        self.assertEqual(
+            m.metrics['XX']['SKEWNESS_UVCUT'], 0.43736719426371656)
+        self.assertEqual(
+            m.metrics['XX']['RMS_AMPVAR_FREQ'], 0.007612282633781024)
+        self.assertEqual(
+            m.metrics['XX']['RMS_AMPVAR_ANT'], 0.009989448240765735)
+        self.assertEqual(m.metrics['XX']['DFFT_POWER'], 104927.04311607413)
+        self.assertEqual(
+            m.metrics['XX']['DFFT_POWER_HIGH_PKPL'], 37198.700459188025)
+        self.assertEqual(
+            m.metrics['XX']['DFFT_POWER_HIGH_NKPL'], 63044.58844099508)
+        self.assertEqual(
+            m.metrics['XX']['RECEIVER_CHISQVAR'], 8.06865584510551)
 
     def test_write_metrics(self):
-        pass
+        m = CalMetrics(calfile)
+        m._initialize_metrics_dict()
+        outfile = calfile.replace('.fits', '_cal_metrics.json')
+        print(outfile)
+        m.write_to()
+        self.assertTrue(os.path.exists(outfile))
+        os.system('rm -rf {}'.format(outfile))
+        outfile = 'metrics.json'
+        m.write_to(outfile=outfile)
+        self.assertTrue(os.path.exists(outfile))
+        os.system('rm -rf {}'.format(outfile))
