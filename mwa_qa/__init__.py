964b156222bc92cb13b43a96d3e02f7f87732260
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-09-20 11:49:16 +0800

    initialized modules for prepvis metrics

--- mwa_qa/__init__.py
+++ mwa_qa/__init__.py
@@ -3,4 +3,5 @@ __init__.py file from mwa_qa
 """
 
 from mwa_qa import read_image, read_metafits, read_calfits, cal_metrics, \
-    image_metrics, vis_metrics, json_utils, coord_utils, read_uvfits, read_calqa
+    image_metrics, vis_metrics, json_utils, coord_utils, read_uvfits, \
+    read_calqa, prepvis_metrics
