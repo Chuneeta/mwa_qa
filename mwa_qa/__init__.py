80cddd5d7816dd30dd106a767a4da96e0589d872
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-09-29 13:48:22 +0800

    initialize combineqa

--- mwa_qa/__init__.py
+++ mwa_qa/__init__.py
@@ -4,4 +4,4 @@ __init__.py file from mwa_qa
 
 from mwa_qa import read_image, read_metafits, read_calfits, cal_metrics, \
     image_metrics, vis_metrics, json_utils, coord_utils, read_uvfits, \
-    read_calqa, prepvis_metrics
+    read_calqa, prepvis_metrics, combine_qa
