b924a60ed2285a2093a2fcf3216525218b606038
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2022-11-04 18:42:22 +0800

    iniitlaized module to read csv fiels

--- mwa_qa/__init__.py
+++ mwa_qa/__init__.py
@@ -2,6 +2,6 @@
 __init__.py file from mwa_qa
 """
 
-from mwa_qa import read_image, read_metafits, read_calfits, cal_metrics, \
+from mwa_qa import combine_json, prepvis_metrics, read_image, read_metafits, read_calfits, cal_metrics, \
     image_metrics, vis_metrics, json_utils, coord_utils, read_uvfits, \
-    read_calqa, prepvis_metrics, combine_qa
+    read_calqa, read_csv
