3a4268c8d29c85b54f1ce836346b45770f13279e
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2023-01-18 14:18:29 +0800

    added manual flags

--- mwa_qa/prepvis_metrics.py
+++ mwa_qa/prepvis_metrics.py
@@ -2,6 +2,7 @@ from mwa_qa.read_uvfits import UVfits
 from mwa_qa import json_utils as ju
 from collections import OrderedDict
 import numpy as np
+import pylab
 
 pol_dict = {'XX': 0, 'YY': 1, 'XY': 2, 'YX': 3}
 
@@ -11,12 +12,196 @@ class PrepvisMetrics(object):
         self.uvfits_path = uvfits_path
         self.uvf = UVfits(uvfits_path)
 
-    def autos(self):
+    def autos(self, manual_flags, ex_annumbers=[]):
         auto_antpairs = self.uvf.auto_antpairs()
-        autos = self.uvf.data_for_antpairs(auto_antpairs)
-        flags = self.uvf.flag_for_antpairs(auto_antpairs)
+        autos = self.uvf.data_for_antpairs(
+            auto_antpairs)  # (only xx and yy pols)
+        _sh = autos.shape
+        # flags are properly propages for the autocorrelations, therefore
+        # we are mually flagging the band edges and centre frequency only
+        # for obsids below 13*
+        if manual_flags:
+            flags = self._evaluate_edge_flags()
+            flags = flags[np.newaxis, np.newaxis, :, np.newaxis]
+            flags = flags.repeat(_sh[0], axis=0).repeat(
+                _sh[1], axis=1).repeat(_sh[3], axis=3)
+        else:
+            flags = np.zeros((_sh), dtype=bool)
         autos[flags] = np.nan
+        if len(ex_annumbers) > 0:
+            autos[:, ex_annumbers, :, :] = np.nan
         return autos
+
+    def _evaluate_edge_flags(self):
+        edges_ncut = self.uvf.channel_width / 40000.
+        if edges_ncut == 1:
+            flag_chans = [0, 1, 16, 30, 31]
+        elif edges_ncut == 2:
+            flag_chans = [0, 16, 31]
+        else:
+            raise ValueError(
+                'Channel width/frequency resolution not supported')
+        Ncoarse_chans = 32
+        nbands = np.split(np.zeros((self.uvf.Nfreqs), dtype=bool),
+                          int(self.uvf.Nfreqs / Ncoarse_chans))
+        flags = []
+        for band in nbands:
+            band[flag_chans] = True
+            flags.append(band)
+        return np.array(flags).flatten()
+
+    def _plot_mode(self, data, mode):
+        if mode == 'amp':
+            return np.abs(data)
+        elif mode == 'phs':
+            return np.angle(data)
+        elif mode == 'real':
+            return np.real(data)
+        elif mode == 'imag':
+            return np.imag(data)
+        elif mode == 'log':
+            return np.log10(np.abs(data))
+        else:
+            raise ValueError("mode {} is not recognized".format(mode))
+
+    def plot_spectra_across_chan(self, freq_chan, mode='log',
+                                 manual_flags=True, ex_annumbers=[], save=None, figname=None):
+        autos = self.autos(manual_flags=manual_flags,
+                           ex_annumbers=ex_annumbers)
+        _sh = autos.shape
+        plot_data = self._plot_mode(
+            autos[:, :, freq_chan, :], mode=mode)
+        fig = pylab.figure(figsize=(9, 7))
+        pylab.suptitle('Channel {}'.format(freq_chan))
+        for i in range(4):
+            ax = pylab.subplot(2, 2, i + 1)
+            for j in range(_sh[1]):
+                ax.plot(plot_data[:, j, i], '.-', alpha=0.5)
+            ax.set_title(list(pol_dict.keys())[i])
+            ax.grid(ls='dotted')
+            if i % 2 == 0:
+                pylab.ylabel(mode, fontsize=12)
+            if i // 2 > 0:
+                pylab.xlabel('Timestamp', fontsize=12)
+        pylab.subplots_adjust(hspace=0.2, wspace=0.3)
+        if save:
+            if figname is None:
+                outfile = self.uvfits_path.replace(
+                    '.uvfits', '_f{}_{}_{}.png'.format(freq_chan, mode, pol))
+            else:
+                outfile = figname
+            pylab.savefig(outfile)
+        else:
+            pylab.show()
+
+    def plot_spectra_across_time(self, timestamp, mode='log',
+                                 manual_flags=True, ex_annumbers=[], save=None, figname=None):
+        autos = self.autos(manual_flags=manual_flags,
+                           ex_annumbers=ex_annumbers)
+        _sh = autos.shape
+        plot_data = self._plot_mode(
+            autos[timestamp, :, :, :], mode=mode)
+        fig = pylab.figure(figsize=(9, 7))
+        pylab.suptitle('Timetamp {}'.format(timestamp))
+        for i in range(4):
+            ax = pylab.subplot(2, 2, i + 1)
+            for j in range(_sh[1]):
+                ax.plot(plot_data[j, :, i], '.-', alpha=0.5)
+            ax.set_title(list(pol_dict.keys())[i])
+            ax.grid(ls='dotted')
+            if i % 2 == 0:
+                pylab.ylabel(mode, fontsize=12)
+            if i // 2 > 0:
+                pylab.xlabel('Frequency channel', fontsize=12)
+        pylab.subplots_adjust(hspace=0.2, wspace=0.3)
+        if save:
+            if figname is None:
+                outfile = self.uvfits_path.replace(
+                    '.uvfits', '_t{}_{}_{}.png'.format(timestamp, mode, pol))
+            else:
+                outfile = figname
+            pylab.savefig(outfile)
+        else:
+            pylab.show()
+
+    def plot_spectra_2D(self, annumber, manual_flags=True, mode='log',
+                        save=None, figname=None):
+        autos = self.autos(manual_flags=manual_flags)
+        plot_data = self._plot_mode(
+            autos[:, annumber, :, :], mode=mode)
+        fig = pylab.figure(figsize=(8, 7))
+        pylab.suptitle('Ant {} -- {}'.format(annumber, mode))
+        for i in range(4):
+            ax = pylab.subplot(2, 2, i + 1)
+            vmin = np.nanmin(plot_data[:, :, i])
+            vmax = np.nanmax(plot_data[:, :, i])
+            im = ax.imshow(plot_data[:, :, i],
+                           aspect='auto', vmin=vmin, vmax=vmax)
+            ax.set_title(list(pol_dict.keys())[i])
+            pylab.colorbar(im)
+            if i % 2 == 0:
+                pylab.ylabel('Timestamp', fontsize=12)
+            if i // 2 > 0:
+                pylab.xlabel('Frequency Channel', fontsize=12)
+        pylab.subplots_adjust(hspace=0.2, wspace=0.3)
+        if save:
+            if figname is None:
+                outfile = self.uvfits_path.replace(
+                    '.uvfits', '_{}_{}.png'.format(mode, pol))
+            else:
+                outfile = figname
+            pylab.savefig(outfile)
+        else:
+            pylab.show()
+
+    def create_time_chan_mask(self, autos, threshold):
+        mask = np.ones(autos.shape, dtype=bool)
+        _sh = autos.shape
+        # calculating z-score across time axis
+        for p in range(_sh[-1]):
+            for i in range(_sh[1]):
+                spectra = np.abs(autos[:, i, :, p])
+                diff_spectra = np.diff(spectra, axis=0)
+                zscore = diff_spectra - \
+                    np.nanmean(diff_spectra) / np.nanstd(diff_spectra)
+                # discarding between -threshold and _threshold
+                inds = np.where((zscore > -threshold)
+                                | (zscore < threshold))
+                mask[inds] = 0
+        return mask
+
+    def calculate_mod_zscore(self, data):
+        median_data = np.nanmedian(data, axis=0)
+        diff_data = data - median_data
+        mad = np.nanmedian(np.abs(diff_data), axis=0)
+        mod_zscore = diff_data / mad
+        return mod_zscore
+        # discarding between -threshold and _threshold
+        # inds = np.where((modified_zscore > -threshold)
+        #                   | (modified_zscore < threshold))
+
+    def iterative_mod_zscore(self, data, threshold, niter=10):
+        bad_ants = []
+        mod_zscore = self.calculate_mod_zscore(data)
+        fig = pylab.figure()
+        pylab.plot(mod_zscore, '.-')
+        pylab.xlabel('Antenna Number')
+        pylab.ylabel('Modified z-score')
+        inds = np.where((mod_zscore < -threshold)
+                        | (mod_zscore > threshold))
+        print(inds)
+        count = 1
+        while (count <= niter and len(inds[0]) > 0):
+            bad_ants.append(inds[0])
+            data[inds[0]] = np.nan
+            mod_zscore = self.calculate_mod_zscore(data)
+            pylab.plot(mod_zscore, '.-')
+            pylab.show()
+            inds = np.where(np.abs(mod_zscore) > threshold)
+            print(inds)
+            count += 1
+        pylab.show()
+        return np.array(bad_ants).flatten()
 
     def _initialize_metrics_dict(self):
         self.metrics = OrderedDict()
@@ -28,49 +213,58 @@ class PrepvisMetrics(object):
         self.metrics['XX'] = OrderedDict()
         self.metrics['YY'] = OrderedDict()
 
-    def run_metrics(self, nbl_limit=10):
+    def run_metrics(self, threshold=5, manual_flags=True, ex_annumbers=[]):
         self._initialize_metrics_dict()
         # auto correlations
-        autos = self.autos()
-        autos_amps = np.abs(autos)  # amplitude
+        autos = self.autos(manual_flags=manual_flags,
+                           ex_annumbers=ex_annumbers)
+        # amplitude avergaed over time
+        autos_amps = np.nanmean(np.abs(autos), axis=0)
         _sh = autos_amps.shape
+        # normalizing by the median
+        autos_amps_norm = autos_amps / np.nanmedian(autos_amps, axis=0)
         # rms across antenna
         autos_amps_rms_ant = np.sqrt(
-            np.nansum(autos_amps ** 2, axis=1) / _sh[1])
+            np.nansum(autos_amps_norm ** 2, axis=0) / _sh[0])
         # rms across frequency
         autos_amps_rms_freq = np.sqrt(
-            np.nansum(autos_amps ** 2, axis=2) / _sh[2])
-
+            np.nansum(autos_amps_norm ** 2, axis=1) / _sh[1])
         for p in ['XX', 'YY']:
-            # taking the maximum rms out of the timestamps
-            rms_amp_ant = np.nanmean(
-            autos_amps_rms_ant[:, :, pol_dict[p]], axis=0)
-            autos_amps_rms_freq[:, :, pol_dict[p]], axis = 0)
-            self.metrics[p]['RMS_AMP_ANT']=rms_amp_ant
-            self.metrics[p]['RMS_AMP_FREQ']=rms_amp_freq
-            self.metrics[p]['MXRMS_AMP_ANT']=np.nanmax(rms_amp_ant)
-            self.metrics[p]['MNRMS_AMP_ANT']=np.nanmin(rms_amp_ant)
-            self.metrics[p]['MXRMS_AMP_FREQ']=np.nanmax(
-                rms_amp_freq)
-            self.metrics[p]['MNRMS_AMP_FREQ']=np.nanmin(
-                rms_amp_freq)
-            # finding outliers in antennas
-            threshold_ant_high=np.nanmedian(
-                rms_amp_freq) + 3 * np.nanstd(rms_amp_freq)
-            threshold_ant_low=np.nanmedian(
-                rms_amp_freq) - 3 * np.nanstd(rms_amp_freq)
-            inds_rms_ant=np.where((rms_amp_freq < threshold_ant_low) | (
-                rms_amp_freq > threshold_ant_high))
-            if len(inds_rms_ant) == 0:
-                self.metrics[p]['POOR_ANTENNAS']=[]
-                self.metrics[p]['NPOOR_ANTENNAS']=0
-            else:
-                self.metrics[p]['POOR_ANTENNAS']=inds_rms_ant[0]
-                self.metrics[p]['NPOOR_ANTENNAS']=len(
-                    inds_rms_ant[0])
+            bad_ants = self.iterative_mod_zscore(
+                autos_amps_rms_freq[:, pol_dict[p]], threshold)
+            print(p, bad_ants)
 
-    def write_to(self, outfile = None):
+        # for p in ['XX', 'YY']:
+        #     # taking the maximum rms out of the timestamps
+        #     rms_amp_ant = np.nanmean(
+        #         autos_amps_rms_ant[:, :, pol_dict[p]], axis=0)
+        #     rms_amp_freq = np.nanmean(
+        #         autos_amps_rms_freq[:, :, pol_dict[p]], axis=0)
+        #     self.metrics[p]['RMS_AMP_ANT'] = rms_amp_ant
+        #     self.metrics[p]['RMS_AMP_FREQ'] = rms_amp_freq
+        #     self.metrics[p]['MXRMS_AMP_ANT'] = np.nanmax(rms_amp_ant)
+        #     self.metrics[p]['MNRMS_AMP_ANT'] = np.nanmin(rms_amp_ant)
+        #     self.metrics[p]['MXRMS_AMP_FREQ'] = np.nanmax(
+        #         rms_amp_freq)
+        #     self.metrics[p]['MNRMS_AMP_FREQ'] = np.nanmin(
+        #         rms_amp_freq)
+        #     # finding outliers in antennas
+        #     threshold_ant_high = np.nanmedian(
+        #         rms_amp_freq) + 3 * np.nanstd(rms_amp_freq)
+        #     threshold_ant_low = np.nanmedian(
+        #         rms_amp_freq) - 3 * np.nanstd(rms_amp_freq)
+        #     inds_rms_ant = np.where((rms_amp_freq < threshold_ant_low) | (
+        #         rms_amp_freq > threshold_ant_high))
+        #     if len(inds_rms_ant) == 0:
+        #         self.metrics[p]['POOR_ANTENNAS'] = []
+        #         self.metrics[p]['NPOOR_ANTENNAS'] = 0
+        #     else:
+        #         self.metrics[p]['POOR_ANTENNAS'] = inds_rms_ant[0]
+        #         self.metrics[p]['NPOOR_ANTENNAS'] = len(
+        #             inds_rms_ant[0])
+
+    def write_to(self, outfile=None):
         if outfile is None:
-            outfile=self.uvfits_path.replace(
+            outfile = self.uvfits_path.replace(
                 '.uvfits', '_prepvis_metrics.json')
         ju.write_metrics(self.metrics, outfile)
