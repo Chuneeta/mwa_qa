c794768234eedd90d64a3d51290086a7c3b74ea3
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2023-02-10 16:47:07 +0800

    altered unittests

--- mwa_qa/tests/test_read_uvfits.py
+++ mwa_qa/tests/test_read_uvfits.py
@@ -1,13 +1,14 @@
+from mwa_qa.data import DATA_PATH
+from mwa_qa.read_uvfits import UVfits
 import unittest
-
+import os
 import numpy as np
 from astropy.io import fits
 from scipy import signal
 
 from mwa_qa.read_uvfits import UVfits
 
-uvfits = '/Users/ridhima/Documents/mwa/calibration/mwa_qa/test_files/1061315688_cal.uvfits'
-uvfits_hex = '/Users/ridhima/Documents/mwa/calibration/mwa_qa/test_files/1320411840_hyp_cal.uvfits'
+uvfits = os.path.join(DATA_PATH, '1062784992.uvfits')
 hdu = fits.open(uvfits)
 data0, data1 = hdu[0].data, hdu[1].data
 hdr0, hdr1 = hdu[0].header, hdu[1].header
@@ -18,43 +19,40 @@ class TestUVfits(unittest.TestCase):
     def test__init__(self):
         uvf = UVfits(uvfits)
         self.assertEqual(uvf.uvfits_path, uvfits)
-        self.assertEqual(uvf.data_array.shape, (219456, 1, 1, 768, 4, 3))
-        np.testing.assert_almost_equal(uvf.data_array[0, 0, :],
-                                       np.array([44222.17 + 5.2869609e-08j,
-                                                 40545.06 - 7.9596754e-07j,
-                                                 0. + 0.0000000e+00j,
-                                                 -1498.523+2.7612256e+02j]), decimal=2)
-        self.assertEqual(uvf.Nfreqs, 768)
+        self.assertEqual(uvf.Nchan, 768)
         self.assertEqual(uvf.Npols, 4)
-        self.assertEqual(uvf.Ntimes, 27)
-        self.assertEqual(uvf.Nbls, 8128)
+        self.assertEqual(uvf.Ntimes, 55)
+        self.assertEqual(uvf.Nbls, 8256)
         self.assertEqual(uvf.Nants, 128)
         self.assertEqual(uvf.channel_width, 40000.0)
         np.testing.assert_equal(uvf.antenna_numbers, np.arange(0, uvf.Nants))
-        self.assertEqual(len(uvf.ant_1_array), ndata)
+        self.assertEqual(len(uvf.ant_1_array), 454080)
         self.assertEqual(uvf.ant_1_array[0], 0)
         self.assertEqual(uvf.ant_1_array[-1], 127)
         self.assertEqual(uvf.ant_2_array[0], 0)
         self.assertEqual(uvf.ant_2_array[-1], 127)
-        self.assertEqual(len(uvf.ant_2_array), ndata)
-        self.assertEqual(len(uvf.baseline_array), ndata)
+        self.assertEqual(len(uvf.ant_2_array), 454080)
+        self.assertEqual(len(uvf.baseline_array), 454080)
         self.assertEqual(len(uvf.antpairs), uvf.Nbls)
         self.assertEqual(len(uvf.antenna_positions), uvf.Nants)
         np.testing.assert_equal(uvf.antpairs[0], np.array([0, 0]))
         np.testing.assert_almost_equal(uvf.antenna_positions[0], np.array([
             4.56250049e+02, -1.49785004e+02,  6.80459899e+01]), decimal=4)
-        self.assertEqual(len(uvf.baseline_array), 219456)
+        self.assertEqual(len(uvf.unique_baselines), 8256)
         self.assertEqual(uvf.baseline_array[0], 257)
         self.assertEqual(uvf.baseline_array[-1], 32896)
         self.assertEqual(uvf.ant_names[0], 'Tile011')
         self.assertEqual(len(uvf.ant_names), uvf.Nants)
-        self.assertEqual(len(uvf.freq_array), uvf.Nfreqs)
+        self.assertEqual(len(uvf.freq_array), uvf.Nchan)
+        self.assertEqual(uvf.obsid, 'high_season1_2456545')
+        self.assertEqual(len(uvf.antenna_numbers), 128)
+        np.testing.assert_equal(uvf.polarization_array,
+                                np.array([-4, -5, -6, -7]))
 
     def test_auto_pairs(self):
         uvf = UVfits(uvfits)
         auto_antpairs = uvf.auto_antpairs()
         expected = [(ant, ant) for ant in range(uvf.Nants)]
-        expected.remove((76, 76))
         np.testing.assert_equal(np.array(auto_antpairs), np.array(expected))
 
     def test_blt_idxs_for_antpair(self):
@@ -66,7 +64,7 @@ class TestUVfits(unittest.TestCase):
         uvf = UVfits(uvfits)
         vis_hdu = hdu['PRIMARY']
         data = uvf._data_for_antpairs(vis_hdu, [(0, 0)])
-        self.assertEqual(data.shape, (27, 1, 768, 4))
+        self.assertEqual(data.shape, (55, 1, 768, 4))
         expected = data0[0][5][0, 0, :, :, 0] + data0[0][5][0, 0, :, :, 1] * 1j
         np.testing.assert_almost_equal(data[0, 0, :, :], expected)
 
@@ -74,47 +72,60 @@ class TestUVfits(unittest.TestCase):
         uvf = UVfits(uvfits)
         vis_hdu = hdu['PRIMARY']
         flag = uvf._flag_for_antpairs(vis_hdu, [(0, 0)])
-        self.assertEqual(flag.shape, (27, 1, 768, 4))
+        self.assertEqual(flag.shape, (55, 1, 768, 4))
         self.assertTrue(np.all(flag[0, 0, 0, :]))
 
     def test_data_for_antpairs(self):
         uvf = UVfits(uvfits)
         data = uvf.data_for_antpairs([(0, 0)])
-        self.assertEqual(data.shape, (27, 1, 768, 4))
+        self.assertEqual(data.shape, (55, 1, 768, 4))
         expected = data0[0][5][0, 0, :, :, 0] + data0[0][5][0, 0, :, :, 1] * 1j
         np.testing.assert_almost_equal(data[0, 0, :, :], expected)
 
     def test_flag_for_antpairs(self):
         uvf = UVfits(uvfits)
         flag = uvf.flag_for_antpairs([(0, 0)])
-        self.assertEqual(flag.shape, (27, 1, 768, 4))
+        self.assertEqual(flag.shape, (55, 1, 768, 4))
         self.assertTrue(np.all(flag[0, 0, 0, :]))
 
     def test_data_for_antpair(self):
         uvf = UVfits(uvfits)
         data = uvf.data_for_antpair((0, 0))
-        self.assertEqual(data.shape, (27, 768, 4))
+        self.assertEqual(data.shape, (55, 768, 4))
         expected = data0[0][5][0, 0, :, :, 0] + data0[0][5][0, 0, :, :, 1] * 1j
         np.testing.assert_almost_equal(data[0, :, :], expected)
 
     def test_flag_for_antpair(self):
         uvf = UVfits(uvfits)
         flag = uvf.flag_for_antpair((0, 0))
-        self.assertEqual(flag.shape, (27, 768, 4))
+        self.assertEqual(flag.shape, (55, 768, 4))
         self.assertTrue(np.all(flag[0, 0, :]))
+
+    def test_amplitude_array(self):
+        uvf = UVfits(uvfits)
+        amps = uvf.amplitude_array([(0, 0)])
+        self.assertEqual(amps.shape, (55, 1, 768))
+        np.testing.assert_almost_equal(amps[0, 0, 0:4], np.array(
+            [44492.473, 43540.79, 42893.27, 42702.707]), decimal=2)
+
+    def test_phase_array(self):
+        uvf = UVfits(uvfits)
+        phs = uvf.phase_array([(0, 0)])
+        self.assertEqual(phs.shape, (55, 1, 768))
+        np.testing.assert_almost_equal(phs[0, 0, 0:4], np.array(
+            [7.0759669e-14,  1.3661892e-11, -4.5692186e-11, -1.7285005e-11]))
 
     def test_blackmanharris(self):
         uvf = UVfits(uvfits)
-        filter = uvf.blackmanharris(uvf.Nfreqs)
+        filter = uvf.blackmanharris(uvf.Nchan)
         np.testing.assert_almost_equal(
             filter, signal.windows.blackmanharris(768))
 
-    def test_delays(self):
+    def test_group_antpairs(self):
         uvf = UVfits(uvfits)
-        delays = uvf.delays()
-        dfreq = uvf.freq_array[1] - uvf.freq_array[0]
-        np.testing.assert_almost_equal(delays * 1e-9, np.fft.fftshift(
-            np.fft.fftfreq(uvf.Nfreqs, dfreq)), decimal=4)
+        angroups = uvf.group_antpairs(uvf.antenna_positions, bl_tol=1e-2)
+        self.assertEqual(len(angroups), 8128)
+        self.assertTrue((angroups[(221, 5442, 378)]),  [(1, 0)])
 
     def redundant_antpairs(self):
         uvf = UVfits(uvfits)
@@ -146,3 +157,29 @@ class TestUVfits(unittest.TestCase):
                                              (63, -1646, 144),
                                              (663, 1564, 1297)]
                          )
+
+    def test_delays(self):
+        uvf = UVfits(uvfits)
+        delays = uvf.delays()
+        dfreq = uvf.freq_array[1] - uvf.freq_array[0]
+        np.testing.assert_almost_equal(delays * 1e-9, np.fft.fftshift(
+            np.fft.fftfreq(uvf.Nchan, dfreq)), decimal=4)
+
+    def test_fft_data_for_antpair(self):
+        uvf = UVfits(uvfits)
+        fft_data = uvf.fft_data_for_antpair((0, 1))
+        self.assertEqual(fft_data.shape, (55, 768, 4))
+        np.testing.assert_almost_equal(fft_data[0, 0, :],
+                                       np.array([-572.2727 - 658.0653j, -571.70917-368.39127j,
+                                                 1309.6108 - 787.1936j, -250.48773-311.11957j]),
+                                       decimal=4)
+
+    def test_fft_data_for_antpairs(self):
+        uvf = UVfits(uvfits)
+        fft_data = uvf.fft_data_for_antpairs([(0, 0), (0, 1)])
+        self.assertEqual(fft_data.shape, (55, 2, 768, 4))
+        np.testing.assert_almost_equal(fft_data[0, 1, 0, :], np.array([-572.2727 - 658.0653j,
+                                                                       -571.70917-368.39127j,
+                                                                       1309.6108 - 787.1936j,
+                                                                       -250.48773-311.11957j]),
+                                       decimal=4)
