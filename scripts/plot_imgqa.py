fc3b3e0f7d2a92a4f0e64500163613f498e1e875
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2023-03-01 11:33:19 +0800

    improved plotting

--- scripts/plot_imgqa.py
+++ scripts/plot_imgqa.py
@@ -45,7 +45,7 @@ bxsize = ut.load_json(args.json[0])['PIX_BOX']
 count = 0
 obsids = []
 for json in args.json:
-    print('Reading {}'.format(json))
+    print('{} Reading {}'.format(count, json))
     data = ut.load_json(json)
     obsids.append(data['XX']['IMAGE_ID'])
     # XX POL
@@ -102,40 +102,39 @@ if args.save:
 fig = pylab.figure(figsize=(12, 7))
 fig.suptitle('RMS', size=16)
 ax = fig.add_subplot(3, 2, 1)
-ax.plot(np.arange(len(obsids)), rms_all_sxx, '.-',
-        color='indianred', linewidth=2, label='XX')
-ax.plot(np.arange(len(obsids)), rms_all_syy, '.-',
-        color='dodgerblue', linewidth=2, label='YY')
-ax.set_ylim(-0.5, max_rms + 1)
+ax.semilogy(np.arange(len(obsids)), rms_all_sxx, '.',
+            color='indianred', linewidth=2, label='XX')
+ax.semilogy(np.arange(len(obsids)), rms_all_syy, '.',
+            color='dodgerblue', linewidth=2, label='YY')
+#ax.set_ylim(3e-2, max_rms + 1)
 ax.legend(loc='upper right', ncol=2)
 ax.grid(ls='dotted')
 ax.set_title('({} x {})'.format(imsize[0], imsize[1]), size=13)
 ax = fig.add_subplot(3, 2, 2)
-ax.plot(np.arange(len(obsids)), rms_box_sxx, '.-',
-        color='indianred', linewidth=2, label='XX')
-ax.plot(np.arange(len(obsids)), rms_box_syy, '.-',
-        color='dodgerblue', linewidth=2, label='YY')
-ax.set_ylim(-0.5, max_rms + 1)
+ax.semilogy(np.arange(len(obsids)), rms_box_sxx, '.',
+            color='indianred', linewidth=2, label='XX')
+ax.semilogy(np.arange(len(obsids)), rms_box_syy, '.',
+            color='dodgerblue', linewidth=2, label='YY')
+#ax.set_ylim(-0.5, max_rms + 1)
 ax.legend(loc='upper right', ncol=2)
 ax.grid(ls='dotted')
 ax.set_title('({} x {})'.format(bxsize[0], bxsize[1]), size=13)
-ax.grid(ls='dotted')
 ax = fig.add_subplot(3, 2, 3)
-ax.plot(np.arange(len(obsids)), rms_all_sv, '.-',
-        color='olivedrab', linewidth=2, label='V')
-ax.set_ylim(-0.5, max_rms_sv + 1)
+ax.semilogy(np.arange(len(obsids)), rms_all_sv, '.',
+            color='olivedrab', linewidth=2, label='V')
+#ax.set_ylim(-0.5, max_rms_sv + 1)
 ax.legend(loc='upper right')
 ax.grid(ls='dotted')
 ax = fig.add_subplot(3, 2, 4)
-ax.plot(np.arange(len(obsids)), rms_box_sv, '.-',
-        color='olivedrab', linewidth=2, label='V')
-ax.set_ylim(-0.5, max_rms_sv + 1)
+ax.semilogy(np.arange(len(obsids)), rms_box_sv, '.',
+            color='olivedrab', linewidth=2, label='V')
+#ax.set_ylim(-0.5, max_rms_sv + 1)
 ax.legend(loc='upper right')
 ax.grid(ls='dotted')
 ax = fig.add_subplot(3, 2, 5)
-ax.plot(np.arange(len(obsids)), ratio_rms_box_sxx, '.-', color='indianred',
+ax.plot(np.arange(len(obsids)), ratio_rms_box_sxx, '.', color='indianred',
         linewidth=2, label='V/XX')
-ax.plot(np.arange(len(obsids)), ratio_rms_box_syy, '.-', color='dodgerblue',
+ax.plot(np.arange(len(obsids)), ratio_rms_box_syy, '.', color='dodgerblue',
         linewidth=2, label='V/YY')
 ax.fill_between(np.arange(len(obsids)), 0.5,
                 max_ratio_rms + 0.2, color='red', alpha=0.2)
@@ -147,7 +146,7 @@ ax.legend(loc='upper right', ncol=2)
 ax.grid(ls='dotted')
 ax.set_xlabel('Observation Number', fontsize=12)
 ax = fig.add_subplot(3, 2, 6)
-ax.plot(np.arange(len(obsids)), ratio_rms_box_sxx, '.-', color='indianred',
+ax.plot(np.arange(len(obsids)), ratio_rms_box_sxx, '.', color='indianred',
         linewidth=2, label='V/XX')
 ax.plot(ratio_rms_box_syy, '.-', color='dodgerblue',
         linewidth=2, label='V/YY')
@@ -167,54 +166,54 @@ if args.save:
 fig = pylab.figure(figsize=(12, 7))
 fig.suptitle('FLUX DENSITY', size=16)
 ax = fig.add_subplot(3, 2, 1)
-ax.plot(pks_pflux_sxx, '*-', color='indianred', linewidth=2, label='XX')
-ax.plot(pks_pflux_syy, '*-', color='dodgerblue', linewidth=2, label='YY')
-ax.set_ylim(-1, max_pks + 3)
+ax.semilogy(pks_pflux_sxx, '.', color='indianred', linewidth=2, label='XX')
+ax.semilogy(pks_pflux_syy, '.', color='dodgerblue', linewidth=2, label='YY')
+#ax.set_ylim(-1, max_pks + 3)
 ax.legend(loc='upper right', ncol=2)
 ax.grid(ls='dotted')
 ax.set_title('PEAK', size=13)
 ax = fig.add_subplot(3, 2, 2)
-ax.plot(pks_tflux_sxx, '*-', color='indianred', linewidth=2, label='XX')
-ax.plot(pks_tflux_syy, '*-', color='dodgerblue', linewidth=2, label='YY')
-ax.set_ylim(-1, max_pks + 3)
+ax.semilogy(pks_tflux_sxx, '.', color='indianred', linewidth=2, label='XX')
+ax.semilogy(pks_tflux_syy, '.', color='dodgerblue', linewidth=2, label='YY')
+#ax.set_ylim(-1, max_pks + 3)
 ax.legend(loc='upper right', ncol=2)
 ax.grid(ls='dotted')
 ax.set_title('INTEGRATED', size=13)
 ax.grid(ls='dotted')
 ax = fig.add_subplot(3, 2, 3)
-ax.plot(pks_pflux_sv, '*-', color='olivedrab', linewidth=2, label='V')
-ax.set_ylim(-1, max_pks_sv + 3)
+ax.semilogy(pks_pflux_sv, '.', color='olivedrab', linewidth=2, label='V')
+#ax.set_ylim(-1, max_pks_sv + 3)
 ax.legend(loc='upper right')
 ax.grid(ls='dotted')
 ax = fig.add_subplot(3, 2, 4)
-ax.plot(pks_tflux_sv, '*-', color='olivedrab', linewidth=2, label='V')
-ax.set_ylim(-1, max_pks_sv + 3)
+ax.semilogy(pks_tflux_sv, '.', color='olivedrab', linewidth=2, label='V')
+#ax.set_ylim(-1, max_pks_sv + 3)
 ax.legend(loc='upper right')
 ax.grid(ls='dotted')
 ax = fig.add_subplot(3, 2, 5)
-ax.plot(ratio_pks_pflux_sxx, '*-', color='indianred',
+ax.plot(ratio_pks_pflux_sxx, '.', color='indianred',
         linewidth=2, label='V/XX')
-ax.plot(ratio_pks_pflux_syy, '*-', color='dodgerblue',
+ax.plot(ratio_pks_pflux_syy, '.', color='dodgerblue',
         linewidth=2, label='V/YY')
-#ax.fill_between(np.arange(len(obsids)), 0.5,
+# ax.fill_between(np.arange(len(obsids)), 0.5,
 #                max_ratio_pks + 0.2, color='red', alpha=0.2)
-#ax.fill_between(np.arange(len(obsids)), 0.3,
+# ax.fill_between(np.arange(len(obsids)), 0.3,
 #                0.5, color='orange', alpha=0.2)
-ax.set_ylim(-0.1, max_ratio_pks + 0.2)
+#ax.set_ylim(-0.1, max_ratio_pks + 0.2)
 ax.set_xlim(0, len(obsids))
 ax.legend(loc='upper right', ncol=2)
 ax.grid(ls='dotted')
 ax.set_xlabel('Observation Number', fontsize=12)
 ax = fig.add_subplot(3, 2, 6)
-ax.plot(ratio_pks_tflux_sxx, '*-', color='indianred',
+ax.plot(ratio_pks_tflux_sxx, '.', color='indianred',
         linewidth=2, label='V/XX')
-ax.plot(ratio_pks_tflux_syy, '*-', color='dodgerblue',
+ax.plot(ratio_pks_tflux_syy, '.', color='dodgerblue',
         linewidth=2, label='V/YY')
-#ax.fill_between(np.arange(len(obsids)), 0.5,
+# ax.fill_between(np.arange(len(obsids)), 0.5,
 #                max_ratio_pks + 0.2, color='red', alpha=0.2)
-#ax.fill_between(np.arange(len(obsids)), 0.3,
+# ax.fill_between(np.arange(len(obsids)), 0.3,
 #                0.5, color='orange', alpha=0.2)
-ax.set_ylim(-0.1, max_ratio_pks + 0.2)
+#ax.set_ylim(-0.1, max_ratio_pks + 0.2)
 ax.set_xlim(0, len(obsids))
 ax.legend(loc='upper right', ncol=2)
 ax.grid(ls='dotted')
