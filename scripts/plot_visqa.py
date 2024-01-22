4130e573e385825a1387a73db66bba3aacfd1f39
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2024-01-22 15:54:23 +0800

    added modz plotting

--- scripts/plot_visqa.py
+++ scripts/plot_visqa.py
@@ -31,42 +31,94 @@ obsid = metrics['OBSID']
 colors = list(mpl.colors.XKCD_COLORS.values())
 amp_chisq_xx = redundant_met['XX']['AMP_CHISQ']
 amp_chisq_yy = redundant_met['YY']['AMP_CHISQ']
+modz_xx = redundant_met['XX']['MODZ']
+modz_yy = redundant_met['YY']['MODZ']
+inds_xx = redundant_met['XX']['POOR_BLS_INDS']
+inds_yy = redundant_met['YY']['POOR_BLS_INDS']
 poor_bls_xx = redundant_met['XX']['POOR_BLS']
 poor_bls_yy = redundant_met['YY']['POOR_BLS']
 red_groups = redundant_met['RED_PAIRS']
-mn_limit = min([min([np.nanmin(csq) for csq in amp_chisq_xx]),
-                min([np.nanmin(csq) for csq in amp_chisq_yy])])
-mx_limit = max([max([np.nanmax(csq) for csq in amp_chisq_xx]),
-                max([np.nanmax(csq) for csq in amp_chisq_yy])])
-red_lengths = [np.linalg.norm(rp) for rp in red_groups]
+# mn_limit = min([min([np.nanmin(csq) for csq in amp_chisq_xx]),
+#                min([np.nanmin(csq) for csq in amp_chisq_yy])])
+# mx_limit = max([max([np.nanmax(csq) for csq in amp_chisq_xx]),
+#                max([np.nanmax(csq) for csq in amp_chisq_yy])])
+# red_lengths = [np.linalg.norm(rp) for rp in red_groups]
 if len(obsid.split('_')) == 1:
     titlename = obsid
 else:
     titlename = ''.join(filter(str.isdigit, args.json))
+
+# plotting modz
+fig = pylab.figure(figsize=(7, 5))
+fig.suptitle(titlename, size=13)
+ax = pylab.subplot(211)
+for i in range(len(amp_chisq_xx)):
+    ax.plot(np.ones((len(amp_chisq_xx[i])))
+            * i, modz_xx[i], '.', color=colors[i], alpha=0.7)
+ax.axhline(y=-3.5, ls='dashed', color='red')
+ax.axhline(y=3.5, ls='dashed', color='red')
+ax.set_ylabel('MOD ZSCORE (XX)')
+ax.grid(ls='dotted')
+ax.set_ylim(-8, 8)
+ax.tick_params(labelsize=10, direction='in', length=4, width=2)
+ax = pylab.subplot(212)
+for i in range(len(amp_chisq_yy)):
+    ax.plot(np.ones((len(amp_chisq_yy[i])))
+            * i, modz_yy[i], '.', color=colors[i], alpha=0.6)
+ax.set_ylabel('MOD ZSCORE (YY)')
+ax.grid(ls='dotted')
+ax.set_ylim(-7, 7)
+ax.axhline(y=-3.5, ls='dashed', color='red')
+ax.axhline(y=3.5, ls='dashed', color='red')
+ax.tick_params(labelsize=10, direction='in', length=4, width=2)
+ax.set_xlabel('Group Number')
+pylab.subplots_adjust(hspace=0, left=0.15)
+if args.save:
+    if args.figname is None:
+        figname = args.json.replace('.json', '_modz.png')
+    else:
+        if args.figname.split('.')[-1] != 'png':
+            print(args.figname)
+            figname = args.figname + '.png'
+        else:
+            figname = args.figname
+
+    pylab.savefig(figname, dpi=args.dpi)
+    pylab.close()
+else:
+    pylab.show()
+
+# plotting chisq
 fig = pylab.figure(figsize=(7, 5))
 fig.suptitle(titlename, size=15)
 ax = pylab.subplot(211)
 for i in range(len(amp_chisq_xx)):
     ax.semilogy(np.ones((len(amp_chisq_xx[i])))
                 * i, amp_chisq_xx[i], '.', color=colors[i], alpha=0.7)
+    if len(inds_xx[i]) > 0:
+        ax.semilogy(np.ones(len(inds_xx[i])) * i,
+                    np.array(amp_chisq_xx[i])[inds_xx[i]], 'rx')
     # inds = [np.array(poor_bls_xx[i])
     # print(inds)
     # ax.semilogy(np.ones((len(poor_bls_xx[i]))) * i,
     # amp_chisq_xx[i][np.array(poor_bls_xx[i])[:, 1]], 's', color = 'r', alpha = 0.4)
 ax.set_ylabel('CHISQ (XX)')
 ax.grid(ls='dotted')
-ax.set_ylim(mn_limit - 50, mx_limit)
+ax.set_ylim(10**1.5, 10**5)
 ax.tick_params(labelsize=10, direction='in', length=4, width=2)
 ax = pylab.subplot(212)
 for i in range(len(amp_chisq_yy)):
     ax.semilogy(np.ones((len(amp_chisq_yy[i])))
                 * i, amp_chisq_yy[i], '.', color=colors[i], alpha=0.6)
+    if len(inds_yy[i]) > 0:
+        ax.semilogy(np.ones(len(inds_yy[i])) * i,
+                    np.array(amp_chisq_yy[i])[inds_yy[i]], 'rx')
 ax.set_ylabel('CHISQ (YY)')
 ax.grid(ls='dotted')
-ax.set_ylim(mn_limit, mx_limit)
+ax.set_ylim(10**1.5, 10**5)
 ax.tick_params(labelsize=10, direction='in', length=4, width=2)
 ax.set_xlabel('Group Number')
-pylab.subplots_adjust(hspace=0.3, left=0.15)
+pylab.subplots_adjust(hspace=0, left=0.15)
 if args.save:
     if args.figname is None:
         figname = args.json.replace('.json', '_rms.png')
