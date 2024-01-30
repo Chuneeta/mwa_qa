4202b46f2fcb631df6030b099d78864cc26294a5
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2024-01-30 14:06:52 +0800

    added cutoof threshold as argument

--- scripts/run_visqa.py
+++ scripts/run_visqa.py
@@ -4,11 +4,13 @@ from pathlib import Path
 from mwa_qa.vis_metrics import VisMetrics
 
 parser = ArgumentParser(description="QA for MWA UVFITS visibility data")
+parser.add_argument('--cutoff_threshold', type=float, default=3,
+                    help='Cutoff threshold value for modified z-score to identify outliers.')
 parser.add_argument('uvfits', type=Path, help='UVFITS visibility file')
 parser.add_argument('--out', help='json output path', type=str,
                     default=None, required=False)
 
 args = parser.parse_args()
-m = VisMetrics(str(args.uvfits))
+m = VisMetrics(str(args.uvfits), args.cutoff_threshold)
 m.run_metrics()
 m.write_to(args.out)
